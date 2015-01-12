6f75a769126924a84d9d95c52cff51797b03c998
Matthias Richter <Matthias.Richter@scieq.net>
2015-01-12 16:44:00 +0100

    changing ALIROOTVERSION to master instead of feature-splitdev after merging the 'split' branch back to master

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -100,5 +100,5 @@ export DDS_LOCATION="https://github.com/FairRootGroup/DDS.git"
 export DDSVERSION=master
 
 export ALIROOT_LOCATION="http://git.cern.ch/pub/AliRoot"
-export ALIROOTVERSION=feature-splitdev
+export ALIROOTVERSION=master
 
