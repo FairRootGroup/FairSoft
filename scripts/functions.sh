ba33f6da7f55bccfb35ec4f157aecfb24a917a32
Florian Uhlig <f.uhlig@gsi.de>
2015-01-24 19:26:25 +0100

    Remove duplicate functions. Add again two functions which have been removed some time ago by accident.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -279,120 +279,6 @@ check_yes_no() {
 }
 
 #_____________________________________________________________________
-# The function checks if all needed variables are defined in the input file and
-# if only valid values and value combinations are present in the input file.
-function check_variables {
-
-  if [ "$compiler" = "" ]; then
-    echo "*** The compiler definition is not set in the input file."
-    echo "*** Please add the compiler definition in the input file."
-    echo "*** e.g.: compiler=gcc"
-    echo "*** possible values are gcc, Clang, intel" # CC, PGI
-    exit 1
-  else
-    if [ ! "$compiler" = "gcc" -a ! "$compiler" = "Clang" -a ! "$compiler" = "intel" ]; then
-      echo "*** The compiler definition $compiler is not known."
-      echo "*** possible values are gcc, Clang, intel" # CC, PGI
-      echo "*** Please correct the compiler definition in the input file."
-      echo "*** e.g.: compiler=gcc"
-      exit 1
-    fi
-  fi
-  if [ "$debug" = "" ]; then
-    echo "*** The debug definition is not set in the input file."
-    echo "*** Please add the debug definition in the input file."
-    echo "*** e.g.: debug=no or debug=yes"
-    exit 1
-  else
-    check_yes_no debug
-  fi
-  if [ "$optimize" = "" ]; then
-    echo "*** The optimize definition is not set in the input file."
-    echo "*** Please add the optimize definition in the input file."
-    echo "*** e.g.: optimize=no or optimize=yes"
-    exit 1
-  else
-    check_yes_no optimize
-  fi
-  if [ "$geant4_download_install_data_automatic" = "" ]; then
-    echo "*** It is not defined in the input file if the geant4 data should be downloaded automatically."
-    echo "*** Please add the missing definition in the input file."
-    echo "*** e.g.: geant4_download_install_data_automatic=[no/yes]"
-    exit 1
-  else
-    check_yes_no geant4_download_install_data_automatic
-  fi
-  if [ "$geant4_install_data_from_dir" = "" ]; then
-    echo "*** It is not defined in the input file if the geant4 data should be installed from the directory."
-    echo "*** Please add the missing definition in the input file."
-    echo "*** e.g.: geant4_install_data_from_dir=[no/yes]"
-    exit 1
-  else
-    check_yes_no geant4_install_data_from_dir
-  fi
-  if [ "$build_python" = "" ]; then
-    echo "*** It is not defined in the input file if the python bindings should be installed."
-    echo "*** Please add the missing definition in the input file."
-    echo "*** e.g.: build_python=[no/yes]"
-    exit 1
-  else
-    check_yes_no build_python
-  fi
-  if [ "$install_sim" = "" ]; then
-    echo "*** It is not defined in the input file if all tools for simulation should be installed."
-    echo "*** Please add the missing definition in the input file."
-    echo "*** e.g.: install_sim=[no/yes]"
-    exit 1
-  else
-    check_yes_no install_sim
-  fi
-  if [ "$SIMPATH_INSTALL" = "" ]; then
-    echo "*** No installation directory is defined in the input file."
-    echo "*** Please add the missing definition in the input file."
-    echo "*** e.g.: SIMPATH_INSTALL=<installation directory>"
-    exit 1
-  else
-    # expand variables, which could be in the filepath. 
-    # A example is if $PWD is in the path
-    eval SIMPATH_INSTALL=$SIMPATH_INSTALL
-    #check if the user can write to the installation path
-    mkdir -p $SIMPATH_INSTALL
-    if [ $? -ne 0 ]; then
-      echo "Cannot write to the installation directory $SIMPATH_INSTALL."
-      exit 1
-    fi  
-  fi
-  if [ "$debug" = "yes" -a "$optimize" = "yes" ]; then
-    echo "*** The variables \"debug\" and \"otimize\" can't be set both to \"yes\" at the"
-    echo "*** same time. All other combinations yes/no, no/no, and no/yes are valid."
-    echo "*** Please change the definitions in the input file."
-    exit 1 
-  fi
-  if [ "$geant4_download_install_data_automatic" = "yes" -a "$geant4_install_data_from_dir" = "yes" ]; then
-    echo "*** The variables \"geant4_download_install_data_automatic\" and"
-    echo "*** \"geant4_install_data_from_dir\" can't be set both to \"yes\" at the"
-    echo "*** same time. All other combinations yes/no, no/no, and no/yes are valid."
-    echo "*** Please change the definitions in the input file."
-    exit 1 
-  fi
-
-}
-
-#_____________________________________________________________________
-# The function checks if the varibale has either yes or no as value.
-# In case any other value is given the script stops with an error message.
-check_yes_no() {
-  variable=$1
-  eval value=\$$1 #eval forces update of $a which is set to the value of $1
-  if [ ! "$value" = "yes" -a ! "$value" = "no" ]; then
-    echo "*** For the variable $variable only yes or no are allowed."
-    echo "*** Please correct the definition of \"$variable=$value\" in the input file."
-    echo "*** e.g.: $variable=no or $variable=yes"
-    exit 1
-  fi
-}
-
-#_____________________________________________________________________
 function is_in_path {
     # This function checks if a file exists in the $PATH.
     # To do so it uses which.
@@ -455,19 +341,6 @@ function generate_config_cache {
 }
 
 #_____________________________________________________________________
-function generate_config_cache {
-  echo compiler=$compiler > $cache_file
-  echo debug=$debug >> $cache_file
-  echo optimize=$optimize >> $cache_file
-  echo geant4_download_install_data_automatic=$geant4_download_install_data_automatic >> $cache_file
-  echo geant4_install_data_from_dir=$geant4_install_data_from_dir >> $cache_file
-  echo build_python=$build_python >> $cache_file
-  echo install_sim=$install_sim >> $cache_file
-  echo SIMPATH_INSTALL=$SIMPATH_INSTALL >> $cache_file
-  echo platform=$platform >> $cache_file
-}
-
-#_____________________________________________________________________
 function download_file {
       # download the file from the given location using either wget or 
       # curl depending which one is available
@@ -481,4 +354,126 @@ function download_file {
         # -L follow redirections which is needed for boost
         curl -O -L $url    
       fi
+}
+
+function check_library {
+    # The function is inspired by the ROOT configure script and adapted
+    # to run on linux and Mac OS X
+    #
+    # This function will try to find out if a library [$1] contains 64 bit
+    # or 32 bit code. Currently works only for linux and Mac OS X.
+    # The result of the check is stored in lib_is, which should be 
+    # immediately copied or used , since the variable
+    # will be overwritten at next invocation of this function.
+
+    chklibname=$1
+
+    lib_is=0
+
+    # Assert that we got enough arguments
+    if [ $# -ne 1 ];
+    then
+      echo "check_library: not 1 argument"
+      return 1
+    fi
+
+
+    if [ "x`basename $chklibname .a`" != "x`basename $chklibname`" ]; then
+        # we have an archive .a file
+        if [ "$platform" = "linux" ];
+        then
+          objdump -a $1 | grep 'x86-64' > /dev/null 2>& 1
+          ret=$?
+        elif [ "$platform" = "macosx" ];
+        then
+          # 0xfeedfacf is the magic key for 64bit files
+          otool -h $1 | grep '0xfeedfacf' > /dev/null 2>& 1
+          ret=$?
+        elif [ "$platform" = "solaris" ];
+        then
+          # Since I don't have a 64bit machine in can only test for 32bit
+          # because i don't now the result on a 64bit machine.
+          /usr/sfw/bin/gobjdump -a $1 | grep 'elf32-i386' > /dev/null 2>& 1
+          ret1=$?
+          if [ $ret1 -eq 0 ];
+          then
+            ret=1
+          else
+            ret=0
+          fi
+        fi
+    else
+        if [ "$platform" = "solaris" ];
+        then
+           file $1 | grep '64-Bit' > /dev/null 2>& 1
+           ret=$?
+           if [ $ret -eq 1 ];then
+             file $1 | grep '64-bit' > /dev/null 2>& 1
+             ret=$?
+           fi
+        else
+           file -L $1 | grep '64-bit' > /dev/null 2>& 1
+           ret=$?
+        fi
+    fi
+    if [ $ret -eq 0 ];
+    then
+      lib_is=64bit
+    else
+      lib_is=32bit
+    fi
+
+}
+
+function check_all_libraries {
+    # This function loops over all libraries in the given path and
+    # checks if the library contains code as given in $system.
+
+    # Assert that we got enough arguments
+    if [ $# -ne 1 ];
+    then
+      echo "check_all_libraries: not 1 argument"
+      return 1
+    fi
+
+    chkdirname=$1
+    echo "**** Checking libraries in $chkdirname ****" | tee -a $logfile_lib
+
+    if [ "$platform" = "linux" -o "$platform" = "solaris" ];
+     then
+       shared_ext=so
+     elif [ "$platform" = "macosx" ];
+     then
+       shared_ext=dylib
+     fi
+
+    oldpwd=$(pwd)
+    cd $chkdirname
+    if [ "$(find . -name "lib*.$shared_ext" | wc -l)" != "0" ];
+    then
+        for file in $(ls *.$shared_ext);
+        do
+          check_library $file
+          if [ "$lib_is" != "$system" ];
+              then
+              echo "Library $file is $lib_is, but system is $system" | tee -a $logfile_lib
+#      else
+#        echo "Library $file is ok" | tee -a $logfile_lib
+          fi
+        done
+    fi
+    if [ "$(find . -name "lib*.a" | wc -l)" != "0" ];
+    then
+        for file in $(ls lib*.a);
+          do
+          check_library $file
+          if [ "$lib_is" != "$system" ];
+              then
+              echo "Library $file is $lib_is, but system is $system" | tee -a $logfile_lib
+#      else
+#        echo "Library $file is ok" | tee -a $logfile_lib
+          fi
+        done
+    fi
+    cd $oldpwd
 }
