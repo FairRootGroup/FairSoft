555feb5ebd37455151c8a7c80a334dda40bf7ef7
Andrey Ustyuzhanin <andrey.ustyuzhanin@cern.ch>
2015-02-27 08:07:25 +0400

    auto_build config by properties

--- auto_build.sh
+++ auto_build.sh
@@ -9,21 +9,24 @@ function halt() {
 
 [ "$1" == "true" ] && FORCE=true
 [ -z "$SIMPATH" ] && halt "SIMPATH is not defined"
-[ -d $SIMPATH && ! $FORCE ] && halt "$SIMPATH directory already exists"
+[[ -d $SIMPATH && ! $FORCE ]] && halt "$SIMPATH directory already exists"
 [ ! -d $SIMPATH ] && mkdir -p $SIMPATH
 [ -f config.cache ] && rm config.cache
+CONF="auto_build.conf"
 
-cat > answers.txt << EOT
-1
-3
-1
-2
-1
-$SIMPATH
-2
+cat > $CONF << EOT
+compiler=gcc
+debug=no
+optimize=yes
+geant4_download_install_data_automatic=yes
+geant4_install_data_from_dir=no
+build_python=yes
+install_sim=yes
+SIMPATH_INSTALL=$SIMPATH
+platform=linux
 EOT
 
-./configure.sh < answers.txt || halt "Error running configure.sh"
+./configure.sh $CONF || halt "Error running configure.sh"
 ./make_clean.sh all || halt "Error running make_clean.sh"
 cat .gitignore | xargs rm -rf
-rm answers.txt
+rm $CONF
