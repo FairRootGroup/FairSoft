b7bee9882325605fd76785c1bfd0966d53f83414
Thomas Ruf <Thomas.Ruf@cern.ch>
2015-10-20 17:51:27 +0200

    fix warning about integer expression expected

--- scripts/install_pdfset.sh
+++ scripts/install_pdfset.sh
@@ -12,24 +12,23 @@ fi
 
 cd $install_data
 
-pdfset=("cteq6ll" "cteq66a0" "cteq66a1" "cteq66a2" "cteq66a3" "cteq4m")
+pdfset=("cteq6ll" "cteq66a" "cteq66a0" "cteq4m")
 
 START=0
 END=${#pdfset[@]}
 
 for (( t=$START; t<$END; t++ )); do
   pdf=${pdfset[${t}]}
-  echo "debug pdf "$pdf
-
-  if [ -f "$pdf.LHgrid" -o -f "$pdf.LHpdf" ];then
+  if [ ! "$pdf" = "cteq66a" ]; then  
+   if [ -f "$pdf.LHgrid" -o -f "$pdf.LHpdf" ];then
     echo $pdf already installed
-  else
+   else
     tt=$($SIMPATH_INSTALL/bin/lhapdf-getdata $pdf --dest=$install_data)
-    echo "debug "$tt
     if [ "$tt" -eq "0" ]; then
       echo cannot find $pdf 
     fi
-  fi
+   fi
+  fi 
 done
 
 cd $SIMPATH
