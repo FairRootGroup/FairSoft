8540d23bd3403467d5a97b7528e683bba1f3868d
Dmytro Kresan <D.Kresan@gsi.de>
2015-11-17 14:55:44 +0100

    Fix parsing error in Xcode 7 patch for Geant4

--- scripts/install_geant4.sh
+++ scripts/install_geant4.sh
@@ -47,7 +47,7 @@ then
     mypatch ../geant4.10.00.p02_cmake.patch
     if [ "$compiler" = "Clang" ]; then
       if clang --version | grep -q "version 7" ; then
-        patch cmake/Modules/Geant4MakeRules_cxx.cmake ../geant4.10.00_clang7_osx.patch
+        mypatch ../geant4.10.00_clang7_osx.patch
       fi
     fi
   fi
