85d5c4b25e11d8f8165294db56f3543df68de0d9
Florian Uhlig <f.uhlig@gsi.de>
2015-11-25 16:16:21 +0100

    Fix compilation on 32bit machines.

--- scripts/install_geant3.sh
+++ scripts/install_geant3.sh
@@ -55,13 +55,15 @@ then
     mypatch ../geant3_root6.patch
   fi  
 
+  mypatch ../Geant3_32bit.patch
+  
   mkdir build
   cd build
   cmake .. -DCMAKE_INSTALL_PREFIX=$SIMPATH_INSTALL \
            -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC \
            -DROOT_DIR=$SIMPATH_INSTALL
 
-  make install -j$number_of_processes
+  make install -j$number_of_processes VERBOSE=1
 
   if [ "$platform" = "macosx" ];
   then
