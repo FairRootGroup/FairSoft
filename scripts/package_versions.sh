40edae89dd8db9b953b8f8b4758935d2ed1131e8
Mohammad Al-Turany <m.al-turany@gsi.de>
2017-10-10 13:22:11 +0200

    issue #57

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -67,9 +67,9 @@ export MILLEPEDE_VERSION=V04-03-04
 export SODIUM_LOCATION="https://github.com/jedisct1/libsodium"
 export SODIUMBRANCH=1.0.3
 
-export ZEROMQVERSION=4.2.0
+export ZEROMQVERSION=4.2.2
 export ZEROMQ_LOCATION="https://github.com/zeromq/libzmq/releases/download/v$ZEROMQVERSION"
-export ZEROMQDIR=4.2.0
+export ZEROMQDIR=4.2.2
 
 export PROTOBUF_LOCATION="https://github.com/google/protobuf/releases/download/v3.4.0"
 export PROTOBUF_VERSION=3.4.0
