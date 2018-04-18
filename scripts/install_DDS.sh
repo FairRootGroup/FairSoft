fd3581d9af19411acaeafa2aa3d010c711c5cc38
Dennis Klein <d.klein@gsi.de>
2018-04-18 17:27:41 +0200

    Install DDS to $SIMPATH_INSTALL

--- scripts/install_DDS.sh
+++ scripts/install_DDS.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
+cd $SIMPATH/basics
 
-if [ ! -d  $SIMPATH/DDS ];
+if [ ! -d $SIMPATH/basics/DDS ];
 then
-  cd $SIMPATH
   git clone $DDS_LOCATION
 fi
 
-install_prefix=$SIMPATH_INSTALL/DDS
-
- checkfile=$install_prefix/bin/dds-server
+checkfile=$SIMPATH_INSTALL/bin/dds-server
 
 if (not_there DDS $checkfile);
 then
   cd DDS
   git checkout $DDSVERSION
-  if [ ! -d  build_for_alfa ];
+  if [ ! -d  build ];
   then
-    mkdir build_for_alfa
+    mkdir build
   fi
-  cd build_for_alfa
-  BOOST_ROOT=$SIMPATH_INSTALL cmake -DCMAKE_INSTALL_PREFIX=$install_prefix  -C ../BuildSetup.cmake ../
+  cd build
+  BOOST_ROOT=$SIMPATH_INSTALL cmake -DCMAKE_INSTALL_PREFIX=$SIMPATH_INSTALL -C ../BuildSetup.cmake ../
   $MAKE_command -j$number_of_processes wn_bin
   $MAKE_command -j$number_of_processes install
-
 fi
 
 cd $SIMPATH
