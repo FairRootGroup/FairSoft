81fa4be71aa62247e16f294c9e184348ba91f3ac
Dennis Klein <d.klein@gsi.de>
2018-05-03 20:35:30 +0200

    Bump FairLogger and FairMQ versions

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -126,4 +126,4 @@ export FAIRLOGGER_LOCATION="https://github.com/FairRootGroup/FairLogger"
 export FAIRLOGGER_VERSION=v1.1.0
 
 export FAIRMQ_LOCATION="https://github.com/FairRootGroup/FairMQ"
-export FAIRMQ_VERSION=v1.1.7
+export FAIRMQ_VERSION=v1.2.0
