5f8719eadb8be4160f198851c4cabb103298831b
Mohammad Al-Turany <m.al-turany@gsi.de>
2018-05-04 15:32:00 +0200

    new versions of many packages

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CMAKE_LOCATION="http://www.cmake.org/files/v3.9/"
-export CMAKEVERSION_REQUIRED=cmake-3.9.4
-export CMAKEVERSION=cmake-3.9.4
+export CMAKE_LOCATION="http://www.cmake.org/files/v3.11/"
+export CMAKEVERSION_REQUIRED=cmake-3.11.1
+export CMAKEVERSION=cmake-3.11.1
 
 export GTEST_LOCATION="https://github.com/google/googletest/archive/"
 export GTESTVERSION=release-1.7.0
@@ -13,8 +13,8 @@ export GSLVERSION=gsl-1.16
 export ICU_LOCATION="http://download.icu-project.org/files/icu4c/53.1/"
 export ICUVERSION=icu4c-53_1
 
-export BOOST_LOCATION="http://sourceforge.net/projects/boost/files/boost/1.64.0/"
-export BOOSTVERSION=boost_1_64_0
+export BOOST_LOCATION="http://sourceforge.net/projects/boost/files/boost/1.67.0/"
+export BOOSTVERSION=boost_1_67_0
 
 export PYTHIA6_LOCATION="https://root.cern.ch/download/"
 export PYTHIA6VERSION=pythia6
@@ -32,14 +32,14 @@ export MESA_LOCATION="ftp://ftp.freedesktop.org/pub/mesa/older-versions/7.x/7.10
 export MESAVERSION=MesaLib-7.10.3
 
 export GEANT4_LOCATION="http://geant4.cern.ch/support/source/"
-export GEANT4VERSION=geant4.10.02.p01
-export GEANT4VERSIONp=Geant4-10.2.1
+export GEANT4VERSION=geant4.10.04.p01
+export GEANT4VERSIONp=Geant4-10.04.0
 
 #export ROOT_LOCATION="http://root.cern.ch/git/root.git"
 export ROOT_LOCATION="https://github.com/root-project/root"
 #if [ "$build_root6" = "yes" ]; then
   # Root v6.10.00 (commit b630f34)
-export ROOTVERSION=v6-10-08
+export ROOTVERSION=v6-12-06
   #export ROOTHASHVALUE=b630f342fdab71b7297d7bdb73ecfbefd71c884a
 #else
   # Root v5.34.36
@@ -51,15 +51,15 @@ export XROOTDVERSION=4.6.1
 export PLUTO_LOCATION="http://web-docs.gsi.de/%7Ehadeshyp/pluto/v5.37/"
 export PLUTOVERSION=pluto_v5.37
 
-export GEANT3_LOCATION="http://root.cern.ch/git/geant3.git"
-export GEANT3BRANCH=v2-1
+export GEANT3_LOCATION="https://github.com/FairRootGroup/geant3.git"
+export GEANT3BRANCH=v2-5
 
-export VGM_LOCATION="http://svn.code.sf.net/p/vgm/code/tags/"
-export VGMVERSION=v4-3
-export VGMDIR=VGM-4.3.0
+export VGM_LOCATION="https://github.com/vmc-project/vgm"
+export VGMVERSION=v4-4
+export VGMDIR=VGM-4.4.0
 
 export GEANT4VMC_LOCATION="http://root.cern.ch/git/geant4_vmc.git"
-export GEANT4VMCBRANCH=v3-3
+export GEANT4VMCBRANCH=v3-6
 
 export MILLEPEDE_LOCATION="http://svnsrv.desy.de/public/MillepedeII/tags/"
 export MILLEPEDE_VERSION=V04-03-04
@@ -83,14 +83,20 @@ export NANOMSG_LOCATION="https://github.com/nanomsg/nanomsg/archive/"
 #export NANOMSG_LOCATION="http://download.nanomsg.org/"
 export NANOMSG_VERSION=1.0.0
 
-export G4ABLA_VERSION=G4ABLA3.0
-export G4ABLA_TAR=G4ABLA3.0.tar.gz
 
-export G4EMLOW_VERSION=G4EMLOW6.48
-export G4EMLOW_TAR=G4EMLOW.6.48.tar.gz
+  G4TENDL:                  "$GEANT4_ROOT/share/Geant4-10.4.0/data/G4TENDL1.3.2"
 
-export G4ENSDFSTATE_VERSION=G4ENSDFSTATE1.2.1
-export G4ENSDFSTATE_TAR=G4ENSDFSTATE.1.2.1.tar.gz
+export G4TENDL_VERSION=G4TENDL1.3.2
+export G4TENDL_TAR=G4TENDL1.3.2.tar.gz
+
+export G4ABLA_VERSION=G4ABLA3.1
+export G4ABLA_TAR=G4ABLA3.1.tar.gz
+
+export G4EMLOW_VERSION=G4EMLOW7.3
+export G4EMLOW_TAR=G4EMLOW.7.3.tar.gz
+
+export G4ENSDFSTATE_VERSION=G4ENSDFSTATE2.1
+export G4ENSDFSTATE_TAR=G4ENSDFSTATE2.1.tar.gz
 
 export G4NDL_VERSION=G4NDL4.5
 export G4NDL_TAR=G4NDL.4.5.tar.gz
@@ -101,17 +107,17 @@ export G4NEUTRONXS_TAR=G4NEUTRONXS.1.4.tar.gz
 export G4PII_VERSION=G4PII1.3
 export G4PII_TAR=G4PII.1.3.tar.gz
 
-export G4SAIDDATA_VERSION=G4SAIDDATA1.1
-export G4SAIDDATA_TAR=G4SAIDDATA.1.1.tar.gz
+export G4SAIDDATA_VERSION=G4SAIDDATA2.2
+export G4SAIDDATA_TAR=G4SAIDDATA.2.2.tar.gz
 
-export PhotonEvaporation_VERSION=PhotonEvaporation3.2
-export PhotonEvaporation_TAR=G4PhotonEvaporation.3.2.tar.gz
+export PhotonEvaporation_VERSION=G4PhotonEvaporation5.2
+export PhotonEvaporation_TAR=G4PhotonEvaporation.5.2.tar.gz
 
-export RadioactiveDecay_VERSION=RadioactiveDecay4.3.1
-export RadioactiveDecay_TAR=G4RadioactiveDecay.4.3.1.tar.gz
+export RadioactiveDecay_VERSION=G4RadioactiveDecay5.2
+export RadioactiveDecay_TAR=G4RadioactiveDecay5.2.tar.gz
 
-export RealSurface_VERSION=RealSurface1.0
-export RealSurface_TAR=RealSurface.1.0.tar.gz
+export RealSurface_VERSION=RealSurface2.1
+export RealSurface_TAR=RealSurface.2.1.tar.gz
 
 export FAIRROOT_LOCATION="https://github.com/FairRootGroup/FairRoot.git"
 export FAIRROOTVERSION=dev
