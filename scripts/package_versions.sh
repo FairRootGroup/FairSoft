54a644ac0233bb6fdbfd6166ce4b453ebff4491c
Mohammad Al-Turany <m.al-turany@gsi.de>
2018-05-07 11:02:46 +0200

    correct G4 data versoin numbers

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -33,7 +33,7 @@ export MESAVERSION=MesaLib-7.10.3
 
 export GEANT4_LOCATION="http://geant4.cern.ch/support/source/"
 export GEANT4VERSION=geant4.10.04.p01
-export GEANT4VERSIONp=Geant4-10.04.1
+export GEANT4VERSIONp=Geant4-10.4.1
 
 #export ROOT_LOCATION="http://root.cern.ch/git/root.git"
 export ROOT_LOCATION="https://github.com/root-project/root"
@@ -93,8 +93,8 @@ export G4ABLA_TAR=G4ABLA3.1.tar.gz
 export G4EMLOW_VERSION=G4EMLOW7.3
 export G4EMLOW_TAR=G4EMLOW.7.3.tar.gz
 
-export G4ENSDFSTATE_VERSION=G4ENSDFSTATE2.1
-export G4ENSDFSTATE_TAR=G4ENSDFSTATE2.1.tar.gz
+export G4ENSDFSTATE_VERSION=G4ENSDFSTATE2.2
+export G4ENSDFSTATE_TAR=G4ENSDFSTATE2.2.tar.gz
 
 export G4NDL_VERSION=G4NDL4.5
 export G4NDL_TAR=G4NDL.4.5.tar.gz
@@ -105,13 +105,13 @@ export G4NEUTRONXS_TAR=G4NEUTRONXS.1.4.tar.gz
 export G4PII_VERSION=G4PII1.3
 export G4PII_TAR=G4PII.1.3.tar.gz
 
-export G4SAIDDATA_VERSION=G4SAIDDATA2.2
-export G4SAIDDATA_TAR=G4SAIDDATA.2.2.tar.gz
+export G4SAIDDATA_VERSION=G4SAIDDATA1.1
+export G4SAIDDATA_TAR=G4SAIDDATA.1.1.tar.gz
 
-export PhotonEvaporation_VERSION=G4PhotonEvaporation5.2
+export PhotonEvaporation_VERSION=PhotonEvaporation5.2
 export PhotonEvaporation_TAR=G4PhotonEvaporation.5.2.tar.gz
 
-export RadioactiveDecay_VERSION=G4RadioactiveDecay5.2
+export RadioactiveDecay_VERSION=RadioactiveDecay5.2
 export RadioactiveDecay_TAR=G4RadioactiveDecay5.2.tar.gz
 
 export RealSurface_VERSION=RealSurface2.1
