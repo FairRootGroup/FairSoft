b607a8d014bfb052dd7515568a461de9d4112ea7
Florian Uhlig <f.uhlig@gsi.de>
2018-05-08 16:19:07 +0200

    Cleanup of Geant3 installation.
    Remove files which are meanwhile part of our git repository.
    Remove patches which are  meanwhile part of our git repository.
    Fix installation script accordingly.

--- scripts/install_geant3.sh
+++ scripts/install_geant3.sh
@@ -17,46 +17,15 @@ checkfile=$install_prefix/lib/libgeant321.so
 if (not_there Geant3 $checkfile);
 then
 
-#  echo "Switching off optimization when compiling Geant3. Optimization introduces some errors with newer gfortran versions." | tee -a $logfile
-#  CXXFLAGS_BAK=$CXXFLAGS
-#  CFLAGS_BAK=$CFLAGS
-#  FFLAGS_BAK=$FFLAGS
-#  export CXXFLAGS="-g -O0"
-#  export CFLAGS="-g -O0"
-#  export FFLAGS="-g -O0"
-
   mkdir -p $install_prefix/include/TGeant3
 
-  cd $SIMPATH/transport
-  cp gdecay.F geant3/gphys
-  cp gdalet.F geant3/gphys
-  cp gdalitzcbm.F geant3/gphys
+  cd $SIMPATH/transport/geant3
 
-  cd geant3
-  # add support for gcalor. Remove files with dummy functions which are implemented in gcalor.F
-  mkdir gcalor
-  cp ../gcalor.F gcalor
-  rm added/dummies_gcalor.c
+# ????
+#  if [ "$build_root6" = "yes" ]; then
+#    mypatch ../geant3_root6.patch
+#  fi
 
-  # patches needed to compile gcalor and for changes in geane
-  mypatch ../geant3_geane.patch | tee -a $logfile
-  mypatch ../Geant3_CMake.patch | tee -a $logfile
-
-  mypatch ../geant3_structs.patch | tee -a $logfile
-
-  if [ ! -f data/xsneut.dat ];
-  then
-    cp ../xsneut.dat.bz2 data
-    bunzip2 data/xsneut.dat.bz2
-  fi
-  cp ../chetc.dat data
-
-  if [ "$build_root6" = "yes" ]; then
-    mypatch ../geant3_root6.patch
-  fi  
-
-  mypatch ../Geant3_32bit.patch
-  
   mkdir build
   cd build
   cmake -DCMAKE_INSTALL_PREFIX=$SIMPATH_INSTALL \
@@ -81,10 +50,6 @@ then
 
   check_success Geant3 $checkfile
   check=$?
-
-#  export CXXFLAGS=$CXXFLAGS_BAK
-#  export CFLAGS=$CFLAGS_BAK
-#  export FFLAGS=$FFLAGS_BAK
 
 fi
 
