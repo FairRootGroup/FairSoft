5943fc5884eb399ee6124613d2b8b188756523dd
Dennis Klein <d.klein@gsi.de>
2018-11-28 13:27:14 +0100

    Bump OFI to 1.6.2

--- scripts/package_versions.sh
+++ scripts/package_versions.sh
@@ -134,7 +134,7 @@ export FAIRMQ_VERSION=v1.2.7
 
 export OFI_LOCATION="https://github.com/ofiwg/libfabric"
 export OFI_TESTS_LOCATION="https://github.com/ofiwg/fabtests"
-export OFI_VERSION=v1.6.1
+export OFI_VERSION=v1.6.2
 
 export ASIOFI_LOCATION="https://github.com/FairRootGroup/asiofi"
 export ASIOFI_VERSION=master
