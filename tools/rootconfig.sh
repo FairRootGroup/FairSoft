9f6a3c8191814799649f3d206609ed1e2d580e13
Florian Uhlig <f.uhlig@gsi.de>
2019-02-06 09:06:40 +0100

    Cleanup ROOT installation

--- tools/rootconfig.sh
+++ tools/rootconfig.sh
@@ -14,32 +14,8 @@
      debugstring=""
    fi
 
-   ########### Xrootd has problems with gcc4.3.0 and 4.3.1
-   ########### Roofit has problems with gcc3.3.5
-   XROOTD="-Dxrootd=ON"
-   export XRDSYS=$SIMPATH_INSTALL
+   XROOTD="-Dxrootd=ON -Dbuiltin_xrootd=ON"
    ROOFIT="-Droofit=ON"
-   if [ "$compiler" = "gcc" ]; then
-     gcc_major_version=$(gcc -dumpversion | cut -c 1)
-     gcc_minor_version=$(gcc -dumpversion | cut -c 3)
-     gcc_sub_version=$(gcc -dumpversion | cut -c 5)
-
-     if [ $gcc_major_version -eq 4 -a $gcc_minor_version -eq 3 ];
-     then
-       XROOTD="-Dxrootd=OFF"
-     fi
-
-     if [ $gcc_major_version -eq 3 -a $gcc_minor_version -eq 3 -a $gcc_sub_version -eq 5 ];
-     then
-       ROOFIT=" "
-     fi
-
-   fi
-
-   if [ $isMacOsx108 ]; then
-     XROOTD="-Dxrootd=OFF"
-   fi
-   #######################################################
 
    OPENGL=" "
    if [ "$compiler" = "Clang" ]; then
@@ -68,7 +44,6 @@
      VC="-Dvc=ON"
    fi
 
-set -xv
    if [ "$platform" = "macosx" ]; then
      clang_version=$(clang --version | head -1 | cut -f 4 -d' ' | cut -f1,2 -d.)
      clang_major_version=$(echo $clang_version | cut -f1 -d.)
@@ -77,7 +52,6 @@ set -xv
        XROOTD="-Dxrootd=OFF"
      fi
    fi
-set +xv
 
    #######################################################
 
