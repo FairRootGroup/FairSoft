eb1d8411c46cb3653bb5e38764f1c10fdfe0b204
Florian Uhlig <f.uhlig@gsi.de>
2019-05-15 14:53:19 +0200

    Add new packages

---
+++ packages/nanomsg/package.py
@@ -0,0 +1,33 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install geant3
+#
+# You can edit this file again by typing:
+#
+#     spack edit geant3
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class Nanomsg(CMakePackage):
+    """The nanomsg library is a simple high-performance implementation of several "scalability protocols"""
+
+    homepage = "https://nanomsg.org/"
+    url = "https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz"
+
+    version('1.0.0', '6f56ef28c93cee644e8c4aaaef7cfb55')
+
