8027bcdcb7545613c71a1e6cbd09e0117598a6d5
Dennis Klein <d.klein@gsi.de>
2019-12-10 19:15:08 +0100

    Improve FairLogger package
    * Fix copyright statement
    * Implement a test phase
    * Remove test executable from install prefix
    * Add myself as maintainer
    * Package additional versions: 1.3, 1.5, 1.6
    * Refactor patches as dynamic patch function
    * Add pkg to CMAKE_PREFIX_PATH in dependent pkg build env
    
    blubb

--- packages/fairlogger/package.py
+++ packages/fairlogger/package.py
@@ -1,40 +1,45 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2019 GSI Helmholtz Centre for Heavy Ion Research
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install geant3
-#
-# You can edit this file again by typing:
-#
-#     spack edit geant3
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
-
+import os
 
 class Fairlogger(CMakePackage):
     """Lightweight and fast C++ Logging Library"""
 
-    # FIXME: Add a proper url for your package's homepage here.
+    phases = ['cmake', 'build', 'test', 'install']
     homepage = "https://github.com/FairRootGroup/FairLogger"
-
-    url      = "https://github.com/fuhlig1/FairLogger/archive/v1.2.0.tar.gz"
+    url = "https://github.com/FairRootGroup/FairLogger/archive/v1.2.0.tar.gz"
+    maintainers = ['dennisklein']
 
     version('1.2.0', '169417786f12411635c670ea3634c880')
+    version('1.3.0', '2d0ef2ea75aab5dbd279e74944b4cbabac681990')
     version('1.4.0', '004029900cde3b6e61191f08288a0fa1')
+    version('1.5.0', '7cb633a9b740efce2b1281dc27e32144bd149594')
+    version('1.6.0', '849ebd128ed2fcaeb806ebf33488189d4557ac7e')
 
+    @on_package_attributes(run_tests=True)
+    def test(self, spec, prefix):
+        Executable(os.path.join(self.build_directory, 'loggerTest'))()
 
-    # add correct version info for FairLoger from github tarball
-    patch('correct_version_info_1.2.0.patch', when='@1.2.0', level=0)
-    patch('correct_version_info_1.4.0.patch', when='@1.4.0', level=0)
+    @run_after('install')
+    def remove_test_executable(self):
+        bin_dir = os.path.join(self.prefix, 'bin')
+        os.remove(os.path.join(bin_dir, 'loggerTest'))
+        try:
+            os.rmdir(bin_dir)
+        except:
+            pass
+
+    def patch(self):
+        """Spack strips the git repository, but the version is determined
+           by querying the git repository. This patches the CMake code to
+           not rely on a successful git query for the version info any more."""
+        filter_file('(get_git_version\(.*)\)',
+                    '\\1 DEFAULT_VERSION %s)' % self.spec.version,
+                    'CMakeLists.txt')
+
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        """Prepend this package to the CMAKE_PREFIX_PATH"""
+        env.prepend_path('CMAKE_PREFIX_PATH', self.prefix)
