2f56952a0a5718a3b76bc255844b331f8c3d536c
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2019-12-10 17:22:08 +0100

    Add some real tests
    - env/dev/{fairlogger,dds,fairmq}.yaml are environments
      with some versions of fairlogger, dds, and fairmq to
      check.
    
    - Add testing tools in test/build*.sh
    
    - Add a few simple tests and environment tests

---
+++ test/buildenv.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+. test/buildsetup.sh
+
+envname="$(echo "${1}" | sed -e 's/^env[/]//' -e 's/[.]yaml$//' -e 's/[^a-z]*//g')"
+
+spack env create $envname "$1" || exit 1
+spack env activate $envname
+ret=$?
+if [ "$ret" = 0 ]
+then
+	spack install
+	ret=$?
+fi
+spack env deactivate
+spack env rm -y $envname
+exit $ret
