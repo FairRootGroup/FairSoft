218b04808364ea86c96d1037f9afb4c15d025a68
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-02-07 19:31:47 +0100

    CI: Generate LABEL if it does not exist
    If we don't have a LABEL in the environment then generate
    one from the system information.
    
    This is especially useful when running outside of jenkins.

--- FairSoft_test.cmake
+++ FairSoft_test.cmake
@@ -11,11 +11,30 @@ set(CTEST_BINARY_DIRECTORY build)
 set(CTEST_TEST_TIMEOUT 10800)
 set(CTEST_USE_LAUNCHERS ON)
 cmake_host_system_information(RESULT fqdn QUERY FQDN)
+cmake_host_system_information(RESULT os_name QUERY OS_NAME)
+cmake_host_system_information(RESULT os_release QUERY OS_RELEASE)
+
 message(STATUS "Running on host: ${fqdn}")
 if ("$ENV{CTEST_SITE}" STREQUAL "")
   set(CTEST_SITE "${fqdn}")
 else()
   set(CTEST_SITE $ENV{CTEST_SITE})
+endif()
+
+if ("$ENV{LABEL}" STREQUAL "")
+    find_program(LSB_RELEASE_EXEC lsb_release)
+    if(NOT LSB_RELEASE_EXEC)
+        message(WARNING "lsb_release not found")
+        set(ENV{LABEL} "${os_name} ${os_release}")
+    else()
+        execute_process(COMMAND ${LSB_RELEASE_EXEC} -si
+            OUTPUT_VARIABLE LSB_DIST_ID
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND ${LSB_RELEASE_EXEC} -sr
+            OUTPUT_VARIABLE LSB_DIST_RELEASE
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        set(ENV{LABEL} "${LSB_DIST_ID} ${LSB_DIST_RELEASE}")
+    endif()
 endif()
 set(CTEST_BUILD_NAME $ENV{LABEL})
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
