b22fca1dcacb31aa4fb740e6c3c478221dabf57f
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-03-26 14:12:00 +0100

    vmc: Small updates
    * Make package info visible to "spack info"
    * Improve root dependency

--- packages/vmc/package.py
+++ packages/vmc/package.py
@@ -7,11 +7,11 @@ from spack import *
 
 
 class Vmc(CMakePackage):
-    # The Virtual Monte Carlo core library.
+    """The Virtual Monte Carlo core library"""
 
     homepage = "https://github.com/vmc-project/vmc"
-    url      = "https://github.com/vmc-project/vmc/archive/v0-1.tar.gz"
+    url      = "https://github.com/vmc-project/vmc/archive/v1-0.tar.gz"
 
     version('1-0', sha256='3da58518b32db1b503082e3205884802a1a263a915071b96e3fd67861db3ca40')
 
-    depends_on('root')
+    depends_on('root@6.18.04:')
