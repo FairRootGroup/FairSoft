d62de39e0d95e5df2f9924bb6a28f1c619fa67b7
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-03-26 13:58:44 +0100

    Update headers / copyright
    * Simplify headers
    * Update copyright

--- packages/vgm/package.py
+++ packages/vgm/package.py
@@ -1,24 +1,7 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install geant3
-#
-# You can edit this file again by typing:
-#
-#     spack edit geant3
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
