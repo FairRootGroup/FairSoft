823f443f8ba347039a8a2bf5c6ac46039edb0bcc
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-03-26 14:04:45 +0100

    vgm: Update sha256sum, dependencies
    * Switch all versions to sha256 (checked that they match
      the current checksums)
    * CMakePackage already depends on cmake, so not needed
    * Small other stuff

--- packages/vgm/package.py
+++ packages/vgm/package.py
@@ -9,17 +9,14 @@ from spack import *
 class Vgm(CMakePackage):
     """Geometry conversion tool, actually providing conversion between Geant4 and ROOT TGeo geometry models."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/vmc-project/vgm"
     url = "https://github.com/vmc-project/vgm/archive/v4-5.tar.gz"
 
-    version('4-4', '42fcd9092981120be1aeb6e3872fa1f7')
-    version('4-5', 'e5054a39a33dcefcd6a6588efceda7be')
-    version('4-6', sha256='6bf0aeef38f357a313e376090b45d3e0713ef9e52ca198075fae8579b8d5a23a')
     version('4-7', sha256='a5f5588db457dc3e6562d1f7da1707960304560fbb0a261559fa3f112a476aea')
+    version('4-6', sha256='6bf0aeef38f357a313e376090b45d3e0713ef9e52ca198075fae8579b8d5a23a')
+    version('4-5', sha256='dc61c6214fdf592dfaa3766eed83cf2bbeabb1755f5146a6d3bcfe55ddbe428f')
+    version('4-4', sha256='a915ff3500daa99b74ce9039fbd8abcbd08051e838a1b337e1d794b73537b33b')
 
-    # FIXME: Add dependencies if required.
-    depends_on('cmake', type='build')
     depends_on("root")
     depends_on("geant4")
 
