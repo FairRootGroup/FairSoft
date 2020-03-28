cf9f45bc9c98b26f9681c4b76b44dca342013b34
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-03-28 10:58:04 +0100

    Create recipe for r3broot
    * r3broot needs patching to find fairlogger
    * Created the basic recipe for it
    * Create an environment for it, basicly copied from
      jun19 nothreads
    * Put environment in unit tests
    
    Notes:
    * The ctest ("make test") has been disabled, because it
      does not work fully.

---
+++ packages/r3broot/package.py
@@ -0,0 +1,44 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+#   Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2020 GSI Helmholtz Centre for Heavy Ion Research GmbH,
+#   Darmstadt, Germany
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class R3broot(CMakePackage):
+    git = 'https://github.com/R3BRootGroup/R3BRoot'
+    homepage = 'https://github.com/R3BRootGroup/R3BRoot'
+
+    version('develop', branch='dev')
+
+    resource(name='macros', when='@develop',
+             git='https://github.com/R3BRootGroup/macros',
+             branch='dev')
+
+    depends_on('fairroot')
+
+    depends_on('fairlogger')
+    depends_on('geant3')
+    depends_on('geant4~threads')
+    depends_on('geant4_vmc')
+    depends_on('pythia6')
+    depends_on('pythia8')
+
+    patch('fairlogger_include.patch')
+
+    def cmake_args(self):
+        args = ['-DCMAKE_CXX_STANDARD=11']
+        return args
+
+    def build(self, spec, prefix):
+        super(R3broot, self).build(spec, prefix)
+        # with working_dir(self.build_directory):
+        #     make("test")
+
+    def setup_build_environment(self, env):
+        env.set("FAIRROOTPATH", self.spec['fairroot'].prefix)
+        env.set("SIMPATH", self.spec["fairroot"].prefix)
+        env.append_path('CPATH', self.spec['fairlogger'].prefix.include)
