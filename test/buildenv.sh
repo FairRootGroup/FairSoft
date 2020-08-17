c79fb08b3ce173c6e8bfcb18c8fceafbf49b3332
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-08-17 15:05:23 +0200

    CI: Fix "environment already exists" Error
    Sometimes we get things like this
    ==> Error: 'junsimspack': environment already exists
    This is because a previous run left an environment around.
    
    So: At the start of building an environment, remove a
    previous one by the same name.
    
    Also remove the trailing "spack" from the env names.

--- test/buildenv.sh
+++ test/buildenv.sh
@@ -2,12 +2,23 @@
 
 . test/buildsetup.sh
 
-envname="$(echo "${1}" | sed -e 's/^env[/]//' -e 's/[.]yaml$//' -e 's/[^a-z]*//g')"
+envname="$(echo "${1}" | \
+	sed \
+		-e 's/\<env[/]//' \
+		-e 's/[.]yaml$//' \
+		-e 's/spack$//' \
+		-e 's/[^a-z]*//g')"
 echo "*** Name for environment .: $envname"
 
 echo "*** Environment file .....: $1"
 echo "*** Contents:"
 sed -e 's/^/    /' "$1"
+
+if spack env list | grep -q "^ *$envname *$"
+then
+	echo "*** Removing old left over environment from previous run"
+	spack env rm -y $envname
+fi
 
 spack env create $envname "$1" || exit 1
 spack env activate $envname
