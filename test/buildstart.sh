aeca79a036c2685f0d1b611667ababdb0618d486
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-08-27 13:55:41 +0200

    CI: Start to handle uninstall for changes
    If we want to re-use / cache a previous install-tree we
    need to remove changed packages from that uninstall-tree.
    
    --> Implement the uninstall.

--- test/buildstart.sh
+++ test/buildstart.sh
@@ -20,9 +20,14 @@ function handle_gitdifflist() {
 				pkg="${filename#repos/*/packages/}"
 				pkg="${pkg%%/*}"
 				echo "***    |   uninstall '$pkg'"
+				spack uninstall --all --dependents -y "$pkg"
 				;;
 			spack|config/*)
 				echo "***    |   spack was changed, needing to wipe everything"
+				( cd "$FS_TEST_INSTALLTREE" && \
+					find . -mindepth 1 -maxdepth 2 -depth \
+						-type d -print0 \
+						| xargs -0 -n 1 -t rm -rf )
 				;;
 			env/*.yaml|Jenkinsfile|CMakeLists.txt|*.cmake|test/*)
 				echo "***    |   Ignoring"
@@ -35,9 +40,11 @@ function handle_gitdifflist() {
 }
 
 
+echo "*** Checking changes and deleting out-dated packages"
+
 if [ -n "$CHANGE_ID" -a -n "$CHANGE_TARGET" ]
 then
-	echo "*** Analyzing list and acting"
+	echo "***    Comparing origin/$CHANGE_TARGET to HEAD"
 	git diff --name-only -z "origin/$CHANGE_TARGET" HEAD -- \
 		| handle_gitdifflist
 fi
