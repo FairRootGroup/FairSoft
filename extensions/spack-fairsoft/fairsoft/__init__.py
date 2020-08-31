4082b143afe222e2944e0b40ec306cbd2d4918f7
Dennis Klein <d.klein@gsi.de>
2020-08-31 17:29:02 +0200

    Introduce spack-fairsoft extension
    * Add extension skeleton with
    * first implementations of
      * spack fairsoft avail
      * spack fairsoft setup

---
+++ extensions/spack-fairsoft/fairsoft/__init__.py
@@ -0,0 +1,135 @@
+# Copyright 2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2020 GSI Helmholtz Centre for Heavy Ion Research GmbH, Darmstadt
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+"""Module that represents a FairSoft git repo
+
+Provides useful infos and operations on it:
+
+root_dir  Abs. path to the root of the FairSoft repo
+env_dir   Abs. path to /env directory
+repos_dir Abs. path to /repos directory
+
+configure_repos()  Enforce correct repo config.
+get_distros()      Return list of '<release>.<variant>' distro names.
+manage_site_config_dir(config_dir)
+    Manage symlinks from spack site config dir to given config_dir entries.
+"""
+
+import os
+
+import llnl.util.tty as tty
+import spack.paths
+from spack.config import config
+from spack.repo import Repo, RepoPath
+
+root_dir = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), *([os.pardir] * 3)))
+env_dir = os.path.join(root_dir, 'env')
+repos_dir = os.path.join(root_dir, 'repos')
+
+
+def _repo_eq(self, other):
+    """TODO: Consider upstreaming as spack.repo.Repo.__eq__()"""
+    return self.root == other.root
+
+
+def _repo_path_eq(self, other):
+    """TODO: Consider upstreaming as spack.repo.RepoPath.__eq__()"""
+    return self.repos == other.repos
+
+
+Repo.__eq__ = _repo_eq  # monkeypatch spack.repo.Repo.__eq__()
+RepoPath.__eq__ = _repo_path_eq  # monkeypatch spack.repo.RepoPath.__eq__()
+
+
+def _update_repos_section(scope, expected):
+    """Update the repos section in given scope to match expected values if needed
+
+    Return True if an update was performed.
+    """
+    actual = config.get('repos', scope=scope) or []
+    if not actual == expected:
+        tty.debug('{}:{} not equal to {} -> updating ...'.format(
+            scope, 'repos', expected))
+        config.update_config('repos', expected, scope=scope)
+        return True
+    return False
+
+
+def configure_repos():
+    """Enforce correct repo config.
+
+    0: fairsoft
+    1: fairsoft-backports
+    2: builtin
+
+    If expected repo config is not present, set it.
+
+    Return True if a config update was performed.
+    """
+    fairsoft_path = os.path.join(repos_dir, 'fairsoft')
+    fairsoft_backports_path = os.path.join(repos_dir, 'fairsoft-backports')
+    builtin_path = '$spack/var/spack/repos/builtin'
+
+    expected = RepoPath(fairsoft_path, fairsoft_backports_path, builtin_path)
+    # We accept if the repo config correct, no matter from which sections it comes
+    actual = RepoPath(*(config.get('repos') or []))
+
+    res = False
+    if not actual == expected:
+        # Now we have to write it somewhere, so we hardcode each section
+        res = _update_repos_section('defaults', [builtin_path]) or res
+        res = _update_repos_section('system', []) or res
+        res = _update_repos_section(
+            'site', [fairsoft_path, fairsoft_backports_path]) or res
+        res = _update_repos_section('user', []) or res
+    return res
+
+
+def get_distros():
+    """Return list of '<release>.<variant>' distro names.
+
+    List is generated from the directory names in `/env/<release>/<variant>`.
+    """
+    envs = []
+    for release in os.listdir(env_dir):
+        for variant in os.listdir(os.path.join(env_dir, release)):
+            envs.append('{}.{}'.format(release.lower(), variant.lower()))
+    return envs
+
+
+def manage_site_config_dir(config_dir):
+    """Manage symlinks from spack site config dir to given config_dir entries."""
+    fairsoft = os.path.abspath(config_dir)
+    site = os.path.join(spack.paths.etc_path, 'spack')
+    tty.debug('Managing symlinks from {} to {}'.format(site, fairsoft))
+
+    def compute_symlink_and_target(site, fairsoft, entry):
+        return [
+            os.path.join(site, entry),
+            os.path.relpath(os.path.join(fairsoft, entry), start=site)
+        ]
+
+    # Remove dangling and unexpected symlinks
+    for entry in os.listdir(site):
+        symlink, target = compute_symlink_and_target(site, fairsoft, entry)
+        if os.path.islink(symlink):
+            readlink = os.readlink(symlink)
+            if readlink != target:
+                tty.debug('Removing unexpected symlink {} to {} (expected {})'.
+                          format(symlink, readlink, target))
+                os.remove(symlink)
+            if not os.path.exists(os.path.abspath(os.path.join(site,
+                                                               readlink))):
+                tty.debug('Removing dangling symlink {} to {}'.format(
+                    symlink, readlink))
+                os.remove(symlink)
+
+    # Add missing symlinks
+    for entry in os.listdir(fairsoft):
+        symlink, target = compute_symlink_and_target(site, fairsoft, entry)
+        if not (os.path.islink(symlink) and os.readlink(symlink) == target):
+            tty.debug('Add missing symlink {} to {}'.format(symlink, target))
+            os.symlink(target, symlink)
---
+++ extensions/spack-fairsoft/fairsoft/cmd/fairsoft.py
@@ -0,0 +1,203 @@
+# Copyright 2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2020 GSI Helmholtz Centre for Heavy Ion Research GmbH, Darmstadt
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import argparse
+import sys
+
+import llnl.util.tty as tty
+from llnl.util.tty.colify import colify
+from spack.cmd.clean import clean
+from spack.cmd.compiler import compiler_find
+from spack.cmd.repo import repo_list
+import spack.extensions.fairsoft as ext
+from spack.util.executable import which
+
+description = 'manage FairSoft distros'
+section = 'FairSoft distro'
+level = 'short'
+
+
+def _spack_clean_ms():
+    """Call `spack clean -ms`"""
+    args = argparse.Namespace()
+    args.specs = False
+    args.stage = True
+    args.downloads = False
+    args.failures = False
+    args.misc_cache = True
+    args.python_cache = False
+    args.all = False
+    clean(None, args)
+
+
+def _spack_compiler_find_scope_site():
+    """Call `spack compiler find --scope site`"""
+    args = argparse.Namespace()
+    args.add_paths = None
+    args.scope = 'site'
+    compiler_find(args)
+
+
+def _spack_repo_list():
+    """Call `spack repo list`"""
+    args = argparse.Namespace()
+    args.scope = None
+    repo_list(args)
+
+
+def avail(_args):
+    """show available FairSoft distros"""
+    distros = sorted(ext.get_distros())
+
+    if sys.stdout.isatty():
+        if not distros:
+            tty.msg('No distros')
+        else:
+            tty.msg('{} distros'.format(len(distros)))
+
+    colify(distros, indent=4, cols=1)
+
+
+def info(_args):
+    """show info on a FairSoft distro"""
+    raise NotImplementedError('NOT YET IMPLEMENTED')
+
+
+def install(_args):
+    """install a FairSoft distro"""
+    raise NotImplementedError('NOT YET IMPLEMENTED')
+
+
+def list(_args):
+    """list installed FairSoft distros"""
+    raise NotImplementedError('NOT YET IMPLEMENTED')
+
+
+def setup(args):
+    """setup spack for FairSoft
+
+    Tasks performed:
+    1. `git submodule update --init`
+    2. `spack compiler find --scope site`
+    3. Manage fairsoft site config dir
+    4. Check/Update repo config
+    5. `spack clean -ms`
+
+    Do not run in parallel to another running spack command!
+    """
+    if not args.skip_git:
+        git = which('git', required=True)
+        git('-C', ext.root_dir, 'submodule', 'update', '--init')
+    if not args.skip_compiler:
+        _spack_compiler_find_scope_site()
+    if args.config_dir:
+        ext.manage_site_config_dir(args.config_dir[0])
+    if not args.skip_repos and ext.configure_repos():
+        tty.info('Updated repo config')
+        _spack_repo_list()
+    if not args.skip_clean:
+        _spack_clean_ms()
+    tty.info('Report problems at https://github.com/FairRootGroup/FairSoft/issues/new')
+
+
+def view(_args):
+    """manage symlink spack views on a FairSoft distro"""
+    raise NotImplementedError('NOT YET IMPLEMENTED')
+
+
+# Map subcommand to action function
+_action = {
+    'avail': avail,
+    'info': info,
+    'install': install,
+    'list': list,
+    'setup': setup,
+    'view': view
+}
+
+
+def _trim_docstring(docstring):
+    """Dedent docstrings as specified by PEP-0257.
+
+    Reference implementation from https://www.python.org/dev/peps/pep-0257/
+
+    `sys.maxint` replaced with `sys.maxsize`.
+    """
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxsize
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxsize:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
+
+def _add_cmd(parsers, cmd):
+    """Helper to add subcmd parser
+
+    Uses the summary line of the action function's docstring
+    as short help text and the body docstring as long
+    description.
+    """
+    docstring = _action[cmd].__doc__
+    summary = _trim_docstring(docstring.splitlines()[0])
+    body = '\n'.join(_trim_docstring(docstring).splitlines()[1:])
+    return parsers.add_parser(cmd,
+                              help=summary,
+                              description=summary,
+                              epilog=body)
+
+
+def setup_parser(parser):
+    """Define CLI subcommands and options"""
+    subcmds = parser.add_subparsers(metavar='SUBCOMMAND',
+                                    dest='fairsoft_subcommand')
+    _add_cmd(subcmds, 'avail')
+
+    _add_cmd(subcmds, 'info')
+
+    _add_cmd(subcmds, 'install')
+
+    _add_cmd(subcmds, 'list')
+
+    setup_cmd = _add_cmd(subcmds, 'setup')
+    setup_cmd.add_argument('--skip-clean',
+                           action='store_true',
+                           help='do not run `spack clean -ms`')
+    setup_cmd.add_argument('--skip-compiler',
+                           action='store_true',
+                           help='do not run `spack compiler find --scope site`')
+    setup_cmd.add_argument('--skip-git',
+                           action='store_true',
+                           help='do not run `git submodule update --init`')
+    setup_cmd.add_argument('--skip-repos',
+                           action='store_true',
+                           help='do not configure repos')
+    setup_cmd.add_argument('--config-dir', nargs=1, default=None,
+                           help='symlink provided config dir')
+
+    _add_cmd(subcmds, 'view')
+
+
+def fairsoft(_parser, args):
+    """Dispatch to SUBCOMMAND action function"""
+    _action[args.fairsoft_subcommand](args)
