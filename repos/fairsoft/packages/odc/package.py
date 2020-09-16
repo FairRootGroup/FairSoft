35c25c61e32668f9bba38adf29bc53688da75953
Dennis Klein <d.klein@gsi.de>
2020-09-16 09:12:51 +0200

    odc: Package 0.8

--- repos/fairsoft/packages/odc/package.py
+++ repos/fairsoft/packages/odc/package.py
@@ -17,13 +17,14 @@ class Odc(CMakePackage):
     generator = 'Ninja'
 
     version('develop', branch='master', get_full_repo=True)
+    version('0.8', tag='0.8', commit='d53a1a4253db17fd52d3904f6b2a5da5904c0d8a')
 
     depends_on('boost@1.67: +log+thread+program_options+filesystem+system+regex')
     conflicts('^boost@1.70:', when='^cmake@:3.14')
     depends_on('protobuf +shared')
     depends_on('grpc +codegen+shared')
-    depends_on('dds@develop')
-    depends_on('fairmq@develop')
+    depends_on('dds@3.5.1:')
+    depends_on('fairmq@1.4.21:')
     depends_on('fairlogger')
 
     depends_on('cmake@3.12:', type='build')
