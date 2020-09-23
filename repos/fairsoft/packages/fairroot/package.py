23b0a77a297db467a1c10d0b7f9d7c131ea7c735
Dennis Klein <d.klein@gsi.de>
2020-09-23 13:56:17 +0200

    fairroot: Package v18.4.1

--- repos/fairsoft/packages/fairroot/package.py
+++ repos/fairsoft/packages/fairroot/package.py
@@ -16,6 +16,7 @@ class Fairroot(CMakePackage):
     git = "https://github.com/FairRootGroup/FairRoot.git"
 
     version('develop', branch='dev')
+    version('18.4.1', sha256='d8455c4bb705a2d5989ad947ffc50bb2f0d00affb649bb5e30d9463b5be0b490')
     version('18.4.0', sha256='97ad86d039db195acf12e9978eb660daab0c91e95e517921bac5a0f157a3e309')
     version('18.2.1', sha256='a9c22965d2d99e385f64c0df1867b477b9c129bcd087ba3b683d0ada6f3d66d0')
 
@@ -47,7 +48,7 @@ class Fairroot(CMakePackage):
 
     patch('cmake_utf8.patch', when='@18.2.1')
     patch('fairlogger_incdir.patch', level=0, when='@18.2.1')
-    patch('find_pythia8_cmake.patch', when='@:18 +sim')
+    patch('find_pythia8_cmake.patch', when='@:18.4.0 +sim')
     patch('support_geant4_with_external_clhep_18.2.patch', when='@18.2 +sim')
     patch('support_geant4_with_external_clhep.patch', when='@18.4 +sim ^Geant4@:10.5')
 
