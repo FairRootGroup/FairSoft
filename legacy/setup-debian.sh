642121e50f9bb9f0ebda428f49d300b11e67bcfb
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-10-06 22:33:38 +0200

    legacy: Move debian setup code into script
    To be able to test the debian setup stuff, move it out of
    the documentation into a script.
    
    Also drop the 32 bit part.
    If you have that, you'll know what to do.

---
+++ legacy/setup-debian.sh
@@ -0,0 +1,10 @@
+apt-get install -y \
+  dialog \
+  cmake cmake-data g++ gcc gfortran \
+  debianutils build-essential make patch sed \
+  libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev \
+  libglu1-mesa-dev libgl1-mesa-dev \
+  libncurses5-dev curl libcurl4-openssl-dev bzip2 libbz2-dev gzip unzip tar \
+  subversion git xutils-dev flex bison lsb-release python-dev \
+  libc6-dev-i386 libxml2-dev wget libssl-dev libkrb5-dev \
+  automake autoconf libtool
