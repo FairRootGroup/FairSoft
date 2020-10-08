32294758891d18bae98a46b15b07117b461a1ba9
Dennis Klein <d.klein@gsi.de>
2020-10-08 14:04:03 +0200

    legacy: Take xerces-c and libicu from the system

--- legacy/setup-ubuntu.sh
+++ legacy/setup-ubuntu.sh
@@ -1,7 +1,7 @@
 apt-get -y --no-install-recommends install autoconf automake binutils \
 bison build-essential bzip2 ca-certificates cmake cmake-data coreutils \
 curl debianutils dialog file findutils flex g++ gcc gfortran git gzip \
-hostname libbz2-dev libcurl4-openssl-dev libgl1-mesa-dev libglu1-mesa-dev \
-libncurses-dev libssl-dev libtool libx11-dev libxext-dev libxft-dev \
+hostname libbz2-dev libcurl4-openssl-dev libicu-dev libgl1-mesa-dev libglu1-mesa-dev \
+libncurses-dev libssl-dev libtool libx11-dev libxerces-c-dev libxext-dev libxft-dev \
 libxml2-dev libxmu-dev libxpm-dev lsb-release make patch python3-dev \
 sed subversion tar unzip wget xutils-dev xz-utils
