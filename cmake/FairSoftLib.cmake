91741af998e9edbdb710c28c0481ca8ccec81d26
Dennis Klein <d.klein@gsi.de>
2020-10-19 12:16:12 +0200

    legacy: Factor out defaults

--- cmake/FairSoftLib.cmake
+++ cmake/FairSoftLib.cmake
@@ -42,3 +42,53 @@ function(pad str width char out)
   endif()
   set(${out} ${str} PARENT_SCOPE)
 endfunction()
+
+macro(set_fairsoft_defaults)
+  # Configure build types
+  set(CMAKE_CONFIGURATION_TYPES "Debug" "Release" "RelWithDebInfo")
+  set(_warnings "-Wshadow -Wall -Wextra -Wpedantic")
+  set(CMAKE_C_FLAGS_DEBUG                "-Og -g ${_warnings}")
+  set(CMAKE_C_FLAGS_RELEASE              "-O2 -DNDEBUG")
+  set(CMAKE_C_FLAGS_RELWITHDEBINFO       "-O2 -g ${_warnings} -DNDEBUG")
+  set(CMAKE_CXX_FLAGS_DEBUG              "-Og -g ${_warnings}")
+  set(CMAKE_CXX_FLAGS_RELEASE            "-O2 -DNDEBUG")
+  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO     "-O2 -g ${_warnings} -DNDEBUG")
+  set(CMAKE_Fortran_FLAGS_DEBUG          "-Og -g ${_warnings}")
+  set(CMAKE_Fortran_FLAGS_RELEASE        "-O2 -DNDEBUG")
+  set(CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-O2 -g ${_warnings} -DNDEBUG")
+  unset(_warnings)
+
+  # Set a default build type
+  if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE RelWithDebInfo)
+  endif()
+
+  # Handle C++ standard level
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  if(NOT CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD ${PROJECT_MIN_CXX_STANDARD})
+  elseif(${CMAKE_CXX_STANDARD} LESS ${PROJECT_MIN_CXX_STANDARD})
+    message(FATAL_ERROR "A minimum CMAKE_CXX_STANDARD of ${PROJECT_MIN_CXX_STANDARD} is required.")
+  endif()
+  set(CMAKE_CXX_EXTENSIONS OFF)
+
+  if(NOT BUILD_SHARED_LIBS)
+    set(BUILD_SHARED_LIBS ON CACHE BOOL "Whether to build shared libraries or static archives")
+  endif()
+
+  # Set -fPIC as default for all library types
+  if(NOT CMAKE_POSITION_INDEPENDENT_CODE)
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+  endif()
+
+  # Generate compile_commands.json file (https://clang.llvm.org/docs/JSONCompilationDatabase.html)
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+  if(NOT DEFINED NCPUS)
+    include(ProcessorCount)
+    ProcessorCount(NCPUS)
+    if(NCPUS EQUAL 0)
+      set(NCPUS 1)
+    endif()
+  endif()
+endmacro()
