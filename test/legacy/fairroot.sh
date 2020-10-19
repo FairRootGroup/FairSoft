fddc60d4384304d6590dc904b8e460af5064e223
Dennis Klein <d.klein@gsi.de>
2020-10-19 11:18:12 +0200

    legacy: Print CMake config summary
    * Fix/Cleanup variable defaults
    * Fix FairRoot test

--- test/legacy/fairroot.sh
+++ test/legacy/fairroot.sh
@@ -4,13 +4,18 @@ set -e
 set -x
 
 export SIMPATH="@CMAKE_INSTALL_PREFIX@"
-git clone -b v18.4.1 https://github.com/FairRootGroup/FairRoot_v18.4.1
-pushd FairRoot_v18.4.1
+version=v18.4.1
+sourcedir=FairRoot_${version}
+git clone -b v18.4.1 https://github.com/FairRootGroup/FairRoot $sourcedir
+pushd $sourcedir
 export FAIRROOTPATH="$(realpath ./install)"
-cmake -S. -Bbuild \
-  -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH
-cmake --build build --target install -j@NCPUS@
+# cmake -S. -Bbuild \
+  # -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH
+# cmake --build build --target install -j @NCPUS@
+mkdir build
 pushd build
+cmake -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH ..
+make install -j @NCPUS@
 ctest --output-on-failure
 popd
 popd
