11f59704b488e1099688d2e226f780140b3c6a5a
Dennis Klein <d.klein@gsi.de>
2020-10-19 05:16:12 +0200

    legacy: Add missing libXmu to fedora image

--- legacy/setup-fedora.sh
+++ legacy/setup-fedora.sh
@@ -5,7 +5,7 @@ gcc-c++ gcc-gfortran gdbm-devel gettext-devel git gperf gsl-devel \
 gzip help2man hostname hwloc-devel m4 make libbsd-devel libtool libicu-devel \
 libunistring-devel libuuid-devel libX11-devel libXau-devel \
 libXdmcp-devel libXext-devel libXfont-devel libXft-devel libxml2-devel \
-libXpm-devel libXrender-devel lz4-devel make mesa-libGL-devel \
+libXmu-devel libXpm-devel libXrender-devel lz4-devel make mesa-libGL-devel \
 mesa-libGLU-devel ncurses-devel openssl-devel patch procps \
 python3-devel readline-devel redhat-lsb-core sed subversion tar \
 tbb-devel unzip wget which xerces-c-devel xz-devel
