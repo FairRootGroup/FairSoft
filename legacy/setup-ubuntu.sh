32996a6974591d54531508696cd3a85ae1a32a5f
Dennis Klein <d.klein@gsi.de>
2020-10-26 08:19:28 +0100

    legacy/ubuntu: Add python-dev pkg

--- legacy/setup-ubuntu.sh
+++ legacy/setup-ubuntu.sh
@@ -3,5 +3,5 @@ bison build-essential bzip2 ca-certificates cmake cmake-data coreutils \
 curl debianutils dialog file findutils flex g++ gcc gfortran git gzip \
 hostname libbz2-dev libcurl4-openssl-dev libicu-dev libgl1-mesa-dev libglu1-mesa-dev \
 libncurses-dev libssl-dev libtool libx11-dev libxerces-c-dev libxext-dev libxft-dev \
-libxml2-dev libxmu-dev libxpm-dev lsb-release make patch python3-dev \
+libxml2-dev libxmu-dev libxpm-dev lsb-release make patch python-dev python3-dev \
 sed subversion tar unzip wget xutils-dev xz-utils
