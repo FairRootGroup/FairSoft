d3b56a3f808daf7183c4a7cc63e6c6bf3ea0dec7
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2020-10-26 16:28:31 +0100

    dds: Add version 3.5.3

--- repos/fairsoft/packages/dds/package.py
+++ repos/fairsoft/packages/dds/package.py
@@ -20,6 +20,7 @@ class Dds(CMakePackage):
     maintainers = ['dennisklein', 'ChristianTackeGSI']
 
     version('develop', branch='master', get_full_repo=True)
+    version('3.5.3', commit='f1eae89fdff266be86ec962c19e1c7930baf002c', no_cache=True)
     version('3.5.2', tag='3.5.2', commit='0813fd5772d1836c055370f4f16d46c961aa0d19', no_cache=True)
     version('3.4', tag='3.4', commit='e0900e946069d840c76e00f29113fd56158fdaa4', no_cache=True)
     version('3.2', tag='3.2', commit='03efdc71eb9aa35091ed1fbc41680c44e2ac7f54', no_cache=True)
@@ -35,6 +36,7 @@ class Dds(CMakePackage):
     patch('fix_wn_bin_2.5-odc.patch', when='@2.5-odc')
     patch('fix_wn_bin_3.0.patch', when='@3.0')
     patch('fix_wn_bin_3.2_3.5.2.patch', when='@3.2:3.5.2')
+    patch('fix_wn_bin_3.5.3.patch', when='@3.5.3')
     patch('fix_wn_bin_master.patch', when='@develop')
     # TODO Upstream the wn_bin fix
     patch('fix_uuid_init.patch', when='@2.5-odc:3.0')
