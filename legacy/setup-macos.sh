acdb268ba7f5754b237257b32970718f14d4f2e1
Dennis Klein <d.klein@gsi.de>
2020-11-09 04:51:26 +0100

    legacy: Add setup script for macOS

---
+++ legacy/setup-macos.sh
@@ -0,0 +1,6 @@
+brew update
+brew install cmake cfitsio coreutils icu4c fftw freetype \
+ftgl gcc gl2ps graphviz grpc gsl libjpeg libpng libtiff \
+lz4 numpy openlibm openssl pcre pkg-config python sqlite \
+tbb xerces-c xrootd xxhash xz zstd
+brew cask install xquartz
