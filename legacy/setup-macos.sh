1bf8408ce71e992d41ae7bdae4c32618100ae37e
Dennis Klein <d.klein@gsi.de>
2020-11-11 01:11:35 +0100

    legacy: Add missing yaml-cpp dependency

--- legacy/setup-macos.sh
+++ legacy/setup-macos.sh
@@ -2,5 +2,5 @@ brew update
 brew install cmake cfitsio coreutils icu4c fftw freetype \
 ftgl gcc gl2ps graphviz grpc gsl libjpeg libpng libtiff \
 lz4 numpy openlibm openssl pcre pkg-config python sqlite \
-tbb xerces-c xrootd xxhash xz zstd
+tbb xerces-c xrootd xxhash xz yaml-cpp zstd
 brew cask install xquartz
