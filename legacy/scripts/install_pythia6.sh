FairRootGroup/FairSoft

commit 04fb2916ac34afb78ff6fb24495f3176c8920b93

Bump pythia6, Pythia8, Geant4, ROOT, geant3, ...
... VGM, geant4_vmc. Introcuce VMC package.

legacy/scripts/install_pythia6.sh
@@ -10,7 +10,7 @@ then
     echo "*** Downloading pythia6 sources ***"
     download_file $PYTHIA6_LOCATION/$PYTHIA6VERSION.tar.gz
   fi
-  untar pythia6 $PYTHIA6VERSION.tar.gz
+  untar pythia6-$PYTHIA6VERSION $PYTHIA6VERSION.tar.gz
 fi
 
 install_prefix=$SIMPATH_INSTALL
@@ -25,8 +25,11 @@ fi
 if (not_there Pythia6 $checkfile);
 then
 
-  cd $SIMPATH/generators/pythia6
-  cp ../CMakeLists.txt_pythia6 CMakeLists.txt
+  cd $SIMPATH/generators/pythia6-$PYTHIA6VERSION
+
+  mypatch ../add_missing_extern_keyword.patch
+
+#  cp ../CMakeLists.txt_pythia6 CMakeLists.txt
   mkdir build
   cd build
 
@@ -51,9 +54,9 @@ then
       fi
   fi
 
-  if [ ! -e libpythia6.so ]; then
-    ln -s libPythia6.so libpythia6.so
-  fi
+#  if [ ! -e libpythia6.so ]; then
+#    ln -s libPythia6.so libpythia6.so
+#  fi
 
   check_all_libraries $install_prefix/lib
 

legacy/configure.sh
@@ -288,6 +288,13 @@ then
   source scripts/install_root6.sh
 fi
 
+##################### VMC ##############################################
+
+if [ "$check" = "1" -a "$onlyreco" = "0" -a "$mqonly" = "0" ];
+then
+  source scripts/install_vmc.sh
+fi
+
 ##################### G4Py #############################################
 
 if [ "$build_python" = "yes" ];

legacy/generators/CMakeLists.txt_pythia6
@@ -1,56 +1,0 @@
-Cmake_Minimum_Required(VERSION 2.6.0 FATAL_ERROR)
-
-Project(Pythia6)
-Enable_Language(Fortran)
-
-
-set(Pythia_FSRCS
-pythia6416.f
-tpythia6_called_from_cc.F
-)
-
-
-# The order here is important because gfortran is contained in pgfortran
-If (CMAKE_Fortran_COMPILER MATCHES "gfortran")
-#  set(UnderScoreString "-fno-second_underscore")
-EndIf (CMAKE_Fortran_COMPILER MATCHES "gfortran")
-If (CMAKE_Fortran_COMPILER MATCHES "pgfortran")
-  set(UnderScoreString "-Mnosecond_underscore")
-EndIf (CMAKE_Fortran_COMPILER MATCHES "pgfortran")
-
-
-#Set_Source_Files_Properties(${Pythia_FSRCS} PROPERTIES COMPILE_FLAGS
-#                           ${UnderScoreString})
-
-# create c source
-Execute_Process(COMMAND echo void MAIN() {} OUTPUT_FILE main.c)
-
-
-set(Pythia_CSRCS
-pythia6_common_address.c
-main.c
-)
-
-Add_Library(Pythia6 SHARED ${Pythia_FSRCS} ${Pythia_CSRCS})
-
-# Check if gfortran library has to specified explicit
-# This is needed so far on Mac OSX and gcc6
-If(CMAKE_SYSTEM_NAME MATCHES Darwin) 
-  Execute_Process(COMMAND gfortran -dumpversion
-                  OUTPUT_VARIABLE _gfortran_version
-                 )
-  String(SUBSTRING ${_gfortran_version} 0 1 _gfortran_major_version)
-  If(${_gfortran_major_version} EQUAL 6)
-    Execute_Process(COMMAND gfortran -print-file-name=libgfortran.dylib 
-                    OUTPUT_VARIABLE _gfortran_path
-                   )
-    Get_Filename_COMPONENT(_gfortran_libdir ${_gfortran_path} DIRECTORY)
-    Message("${_gfortran_libdir}")
-    Target_Link_Libraries(Pythia6 gfortran)
-    Link_Directories(${_gfortran_libdir})
-  EndIf()
-EndIf()
-
-
-Install(TARGETS Pythia6 DESTINATION LIBRARY DESTINATION lib)
-

legacy/generators/add_missing_extern_keyword.patch
@@ -1,0 +1,62 @@
+commit 8c88c93a2002a120ff3535eba0b4b5067b399c4f
+Author: Dennis Klein <d.klein@gsi.de>
+Date:   Mon Aug 17 12:07:10 2020 +0200
+
+    Add missing extern keyword
+    
+    GCC10 linker reports "multiple definition of `pyint1_';" etc without it.
+
+diff --git a/pythia6_common_address.c b/pythia6_common_address.c
+index c9ebcd1..48a6030 100644
+--- pythia6_common_address.c
++++ pythia6_common_address.c_new
+@@ -48,28 +48,28 @@
+ # define pybins PYBINS
+ #endif
+ 
+-int pyjets[2+5*4000+2*2*5*4000];
+-int pydat1[200+2*200+200+2*200];
+-int pydat2[4*500+2*4*500+2*2000+2*4*4];
+-int pydat3[3*500+2*8000+2*8000+5*8000];  /* KNDCAY=8000 */
+-char pydat4[2*500*16];
+-int pydatr[6+2*100];
+-int pysubs[2+500+81*2+2*200];
+-int pypars[200+2*200+200+2*200];
+-int pyint1[400+2*400];
+-int pyint2[500+2*500+2*20*500+2*4*40];
+-int pyint3[2*81*2+3*1000+2*1000];
+-int pyint4[500+2*5*500];
+-int pyint5[1+3*501+2*3*501];
+-char pyint6[501*28];
+-int pyint7[2*6*7*7];
+-int pyint8[2*5*13];
+-int pyint9[2*4*13];
++extern int pyjets[2+5*4000+2*2*5*4000];
++extern int pydat1[200+2*200+200+2*200];
++extern int pydat2[4*500+2*4*500+2*2000+2*4*4];
++extern int pydat3[3*500+2*8000+2*8000+5*8000];  /* KNDCAY=8000 */
++extern char pydat4[2*500*16];
++extern int pydatr[6+2*100];
++extern int pysubs[2+500+81*2+2*200];
++extern int pypars[200+2*200+200+2*200];
++extern int pyint1[400+2*400];
++extern int pyint2[500+2*500+2*20*500+2*4*40];
++extern int pyint3[2*81*2+3*1000+2*1000];
++extern int pyint4[500+2*5*500];
++extern int pyint5[1+3*501+2*3*501];
++extern char pyint6[501*28];
++extern int pyint7[2*6*7*7];
++extern int pyint8[2*5*13];
++extern int pyint9[2*4*13];
+ int pyuppr[1+7*20+1+2*10+2*5*20+2*11]; /* PYUPPR DOES NOT EXIST IN PYTHIA6 AT ALL!!! */
+-int pymssm[100+2*100];
+-int pyssmt[2*4*4+2*2*2+2*2*2+2*4+2*2+2*4*16+2*4*4+2*2*2+2*2*2];
+-int pyints[2*20];
+-int pybins[4+1000+2*20000];
++extern int pymssm[100+2*100];
++extern int pyssmt[2*4*4+2*2*2+2*2*2+2*4+2*2+2*4*16+2*4*4+2*2*2+2*2*2];
++extern int pyints[2*20];
++extern int pybins[4+1000+2*20000];
+ 
+ void *pythia6_common_address(const char* name) {
+    if      (!strcmp(name,"PYJETS")) return pyjets;

legacy/scripts/install_geant3.sh
@@ -33,6 +33,7 @@ then
         -DCMAKE_CXX_COMPILER=$CXX \
         -DCMAKE_C_COMPILER=$CC \
         -DROOT_DIR=$SIMPATH_INSTALL \
+        -DVMC_DIR=$SIMPATH_INSTALL \
         ..
 
   make install -j$number_of_processes

legacy/scripts/install_geant4.sh
@@ -8,10 +8,16 @@
 if [ ! -d  $SIMPATH/transport/geant4 ];
 then
   cd $SIMPATH/transport
-  git clone $GEANT4_LOCATION
-
-  cd geant4
-  git checkout $GEANT4VERSION
+  if [ ! -e geant4-v$GEANT4VERSION.tar.gz ]
+  then
+      echo "*** Downloading Geant4 sources ***"
+      download_file $GEANT4_LOCATION/v$GEANT4VERSION/geant4-v$GEANT4VERSION.tar.gz
+  fi
+  untar geant4-v$GEANT4VERSION geant4-v$GEANT4VERSION.tar.gz
+  if [ -d geant4-v$GEANT4VERSION ]
+  then
+      ln -s geant4-v$GEANT4VERSION geant4
+  fi
 fi
 
 # Full output during compilation and linking to check for the
@@ -90,7 +96,7 @@ then
   then
     # create unique links which is independent of the Geant4 version
     if [ ! -L $install_prefix/share/Geant4 ]; then
-      ln -s $install_prefix/share/$GEANT4VERSIONp $install_prefix/share/Geant4
+      ln -s $install_prefix/share/Geant4-$GEANT4VERSION $install_prefix/share/Geant4
     fi
     # create unique links for the data directories which are
     # independent of the actual data versions
@@ -130,14 +136,14 @@ then
 
   fi
 
-  . $install_prefix/share/$GEANT4VERSIONp/geant4make/geant4make.sh
+  . $install_prefix/share/Geant4-$GEANT4VERSION/geant4make/geant4make.sh
 
   check_success geant4 $checkfile
   check=$?
 
 else
 
-  . $install_prefix/share/$GEANT4VERSIONp/geant4make/geant4make.sh
+  . $install_prefix/share/Geant4-$GEANT4VERSION/geant4make/geant4make.sh
 
 fi
 

legacy/scripts/install_geant4_vmc.sh
@@ -3,10 +3,16 @@
 
 if [ ! -d  $SIMPATH/transport/geant4_vmc ]; then
   cd $SIMPATH/transport
-  git clone $GEANT4VMC_LOCATION
-
-  cd $SIMPATH/transport/geant4_vmc
-  git checkout -b $GEANT4VMCBRANCH $GEANT4VMCBRANCH
+  if [ ! -e v$GEANT4VMCVERSION.tar.gz ];
+  then
+      echo "*** Downloading geant4_vmc sources ***"
+      download_file $GEANT4VMC_LOCATION/v$GEANT4VMCVERSION.tar.gz
+  fi
+  untar geant4_vmc-$GEANT4VMCVERSION v$GEANT4VMCVERSION.tar.gz
+  if [ -d geant4_vmc-$GEANT4VMCVERSION ];
+  then
+      ln -s geant4_vmc-$GEANT4VMCVERSION geant4_vmc
+  fi
 fi
 
 install_prefix=$SIMPATH_INSTALL
@@ -30,6 +36,7 @@ then
         -DGeant4_DIR=$SIMPATH_INSTALL \
         -DROOT_DIR=$SIMPATH_INSTALL \
         -DVGM_DIR=$SIMPATH_INSTALL/lib/$VGMDIR \
+        -DVMC_DIR=$SIMPATH_INSTALL \
         ..
 
   make install -j$number_of_processes

legacy/scripts/install_pythia8.sh
@@ -28,7 +28,7 @@ then
 
   cd $SIMPATH/generators/pythia8
 
-  USRLDFLAGSSHARED="$CXXFLAGS" ./configure  --enable-shared --with-hepmc2=$HEPINSTALLDIR
+  USRLDFLAGSSHARED="$CXXFLAGS" ./configure  --with-hepmc2=$HEPINSTALLDIR
 
   if [ "$compiler" = "PGI" ];
   then

commit fc4e5f078da421509541921c4bc777c0da7df4fc

legacy: Fix regression in Pythia6 install script
introduced by 04fb2916

legacy/scripts/install_pythia6.sh
@@ -15,7 +15,7 @@ fi
 
 install_prefix=$SIMPATH_INSTALL
 
-checkfile=$install_prefix/lib/libPythia6.so
+checkfile=$install_prefix/lib/libpythia6.so
 
 if [ ! -d $install_prefix/lib ];
 then
@@ -43,14 +43,14 @@ then
 
   if [ "$platform" = "macosx" ];
   then
-      cp libPythia6.dylib $install_prefix/lib
+      cp libpythia6.dylib $install_prefix/lib
       cd  $install_prefix/lib
-      for file in $(ls libPythia6.dylib); do
+      for file in $(ls libpythia6.dylib); do
          install_name_tool -id $install_prefix/lib/$file $file
       done
       create_links dylib so
-      if [ ! -e libpythia6.dylib ]; then
-        ln -s libPythia6.dylib libpythia6.dylib
+      if [ ! -e libPythia6.dylib ]; then
+        ln -s libpythia6.dylib libPythia6.dylib
       fi
   fi
 