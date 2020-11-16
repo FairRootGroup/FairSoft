20afaf6b65c70e54c23f096a8f8f587eae6a35df
Dennis Klein <d.klein@gsi.de>
2020-11-16 01:33:33 +0100

    legacy: Update tested systems

---
+++ legacy/setup-centos-7.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+yum -y update
+yum -y install centos-release-scl
+yum-config-manager --enable rhel-server-rhscl-7-rpms
+yum -y install devtoolset-7
+yum -y install epel-release
+yum -y groupinstall "C Development Tools and Libraries"
+yum -y install autoconf automake binutils bison bzip2-devel cmake3 \
+ca-certificates coreutils curl-devel diffutils expat-devel findutils flex \
+gcc-c++ gcc-gfortran gdbm-devel gettext-devel git gperf gsl-devel \
+gzip help2man hostname hwloc-devel m4 make libbsd-devel libtool libicu-devel \
+libunistring-devel libuuid-devel libX11-devel libXau-devel \
+libXdmcp-devel libXext-devel libXfont-devel libXft-devel libxml2-devel \
+libXmu-devel libXpm-devel libXrender-devel lz4-devel make mesa-libGL-devel \
+mesa-libGLU-devel ncurses-devel openssl-devel patch procps \
+python3-devel readline-devel redhat-lsb-core sed subversion tar \
+tbb-devel unzip wget which xerces-c-devel xz-devel yaml-cpp-devel
+yum -y clean all
+
+# This enables devtoolset-7 globally!
+profile="/etc/profile.d/enable-devtoolset-7.sh"
+cat <<EOF > ${profile}
+#!/bin/bash
+source scl_source enable devtoolset-7
+EOF
+chmod a+x "${profile}"
+
+# This enables cmake3 globally!
+alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake3 20 \
+--slave /usr/local/bin/ctest ctest /usr/bin/ctest3 \
+--slave /usr/local/bin/cpack cpack /usr/bin/cpack3 \
+--slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake3 \
+--family cmake
