9b2a4ae2d94774f782e3d88b15946b240d465c13
Dennis Klein <d.klein@gsi.de>
2020-11-16 01:33:33 +0100

    legacy: Update tested systems

--- Jenkinsfile
+++ Jenkinsfile
@@ -106,12 +106,11 @@ pipeline {
         script {
           def ctestcmd = "ctest -VV -S FairSoft_test.cmake"
           def specs_list = [
-            [os: 'Fedora32',         container: 'fedora.32.sif',    for_pr: true],
-            [os: 'Ubuntu-18.04-LTS', container: 'ubuntu.18.04.sif'],
-            [os: 'Ubuntu-20.04-LTS', container: 'ubuntu.20.04.sif', for_pr: true],
+            [os: 'CentOS-7',         container: 'centos.7.sif'],
             [os: 'Debian-10',        container: 'debian.10.sif'],
-            [os: 'GSI-Debian-8', container: 'gsi-debian-8.sif'],
-            [os: 'openSUSE-15.2', container: 'opensuse.15.2.sif'],
+            [os: 'Fedora-33',        container: 'fedora.33.sif',    for_pr: true],
+            [os: 'openSUSE-15.2',    container: 'opensuse.15.2.sif'],
+            [os: 'Ubuntu-20.04-LTS', container: 'ubuntu.20.04.sif', for_pr: true],
           ]
 
           if (env.CHANGE_ID != null) {
--- legacy/setup-debian.sh
+++ legacy/setup-debian.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-apt-get install -y \
-  dialog \
-  cmake cmake-data g++ gcc gfortran \
-  debianutils build-essential make patch sed \
-  libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev \
-  libglu1-mesa-dev libgl1-mesa-dev \
-  libncurses5-dev curl libcurl4-openssl-dev bzip2 libbz2-dev gzip unzip tar \
-  subversion git xutils-dev flex bison lsb-release python-dev \
-  libc6-dev-i386 libxml2-dev wget libssl-dev libkrb5-dev \
-  automake autoconf libtool
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+apt-get -y install autoconf automake binutils \
+bison build-essential bzip2 ca-certificates cmake cmake-data coreutils \
+curl debianutils file findutils flex g++ gcc gfortran git gzip \
+hostname libbz2-dev libcurl4-openssl-dev libicu-dev libgl1-mesa-dev libglu1-mesa-dev \
+libncurses-dev libssl-dev libtool libx11-dev libxerces-c-dev libxext-dev libxft-dev \
+libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev lsb-release make patch python-dev \
+python3-dev sed subversion tar unzip wget xutils-dev xz-utils
+apt-get -y clean
+
