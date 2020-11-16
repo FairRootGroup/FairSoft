33a51d10cb5e9eba84204413b54910a92334e9ac
Dennis Klein <d.klein@gsi.de>
2020-11-16 02:17:10 +0100

    legacy/test: Test FairRoot v18.4.2

--- test/legacy/fairroot.sh
+++ test/legacy/fairroot.sh
@@ -4,18 +4,14 @@ set -e
 set -x
 
 export SIMPATH="@CMAKE_INSTALL_PREFIX@"
-version=v18.4.1
+version=v18.4.2
 sourcedir=FairRoot_${version}
-git clone -b v18.4.1 https://github.com/FairRootGroup/FairRoot $sourcedir
+git clone -b ${version} https://github.com/FairRootGroup/FairRoot $sourcedir
 pushd $sourcedir
 export FAIRROOTPATH="$(realpath ./install)"
-# cmake -S. -Bbuild \
-  # -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH
-# cmake --build build --target install -j @NCPUS@
-mkdir build
+cmake -S. -Bbuild \
+  -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH
+cmake --build build --target install -j @NCPUS@
 pushd build
-cmake -DCMAKE_INSTALL_PREFIX=$FAIRROOTPATH ..
-make install -j @NCPUS@
-ctest --output-on-failure
-popd
+ctest --output-on-failure -j @NCPUS@
 popd
