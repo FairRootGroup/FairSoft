16fb98922101699c9fb0dec5ae5ce39be6316a47
Laurent Audouin <audouin@ipno.in2p3.fr>
2020-12-16 14:34:28 +0100

    Update setup-debian.sh
    rsync required during compilation
    added a line for an updated version of cmake (3.16 is mandatory despite being a very recent version of cmake)

--- legacy/setup-debian.sh
+++ legacy/setup-debian.sh
@@ -4,11 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
 apt-get -y install autoconf automake binutils \
-bison build-essential bzip2 ca-certificates cmake cmake-data coreutils \
+bison build-essential bzip2 ca-certificates cmake-data coreutils \
 curl debianutils file findutils flex g++ gcc gfortran git gzip \
 hostname libbz2-dev libcurl4-openssl-dev libicu-dev libgl1-mesa-dev libglu1-mesa-dev \
 libncurses-dev libssl-dev libtool libx11-dev libxerces-c-dev libxext-dev libxft-dev \
 libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev lsb-release make patch python-dev \
-python3-dev sed subversion tar unzip wget xutils-dev xz-utils
+python3-dev rsync sed subversion tar unzip wget xutils-dev xz-utils
+apt-get -t buster-backports install cmake
 apt-get -y clean
 
