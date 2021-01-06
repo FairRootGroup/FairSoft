37d11684a09bfe75ea65ab51d472195e95621db0
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-01-06 14:52:14 +0100

    Update copyright to 2021

--- repos/fairsoft/packages/odc/package.py
+++ repos/fairsoft/packages/odc/package.py
@@ -1,6 +1,6 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 #   Spack Project Developers. See the top-level COPYRIGHT file for details.
-# Copyright 2020 GSI Helmholtz Centre for Heavy Ion Research GmbH,
+# Copyright 2020-2021 GSI Helmholtz Centre for Heavy Ion Research GmbH,
 #   Darmstadt, Germany
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
