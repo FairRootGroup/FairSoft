83b389a78b4cb7be36a632d7b4a54ff3d3078d9f
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-01-21 16:15:59 +0100

    [legacy] Debian/Ubuntu: Add more deps
    * Add libgsl-dev, libsqlite3-dev
    
    * Also reformat a bit.

--- legacy/setup-ubuntu.sh
+++ legacy/setup-ubuntu.sh
@@ -4,10 +4,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
 apt-get -y install autoconf automake binutils \
-bison build-essential bzip2 ca-certificates cmake cmake-data coreutils \
-curl debianutils file findutils flex g++ gcc gfortran git gzip \
-hostname libbz2-dev libcurl4-openssl-dev libicu-dev libgl1-mesa-dev libglu1-mesa-dev \
-libncurses-dev libssl-dev libtool libx11-dev libxerces-c-dev libxext-dev libxft-dev \
-libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev lsb-release make patch python-dev \
-python3-dev sed subversion tar unzip wget xutils-dev xz-utils
+	bison build-essential bzip2 ca-certificates coreutils \
+	curl debianutils file findutils flex g++ gcc gfortran git gzip \
+	hostname libbz2-dev libcurl4-openssl-dev libgsl-dev libicu-dev \
+	libgl1-mesa-dev libglu1-mesa-dev \
+	libncurses-dev libsqlite3-dev libssl-dev libtool \
+	libx11-dev libxerces-c-dev libxext-dev libxft-dev \
+	libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev lsb-release make patch python-dev \
+	python3-dev rsync sed subversion tar unzip wget xutils-dev xz-utils
+apt-get -y install cmake
 apt-get -y clean
