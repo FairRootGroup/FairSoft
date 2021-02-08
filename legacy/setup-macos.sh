543a80f92e4c875f37f87eaa5006ff0875cfcab1
Dennis Klein <d.klein@gsi.de>
2021-02-08 04:14:13 +0100

    [legacy] macOS: Add more X11 deps

--- legacy/setup-macos.sh
+++ legacy/setup-macos.sh
@@ -3,5 +3,5 @@
 brew update
 brew install cmake cfitsio coreutils davix icu4c fftw freetype \
   ftgl gcc giflib gl2ps glew gpatch graphviz grpc gsl libjpeg libpng \
-  libtiff libxft libxpm lz4 mesa-glu numpy openlibm openssl pcre \
+  libtiff libxfont libxft libxmu libxpm lz4 mesa-glu numpy openlibm openssl pcre \
   pkg-config python sqlite tbb xerces-c xrootd xxhash xz yaml-cpp zstd
