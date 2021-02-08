b428490e4c1d3c9ee8d00c57297ea06269c86199
Dennis Klein <d.klein@gsi.de>
2021-02-08 02:21:52 +0100

    [legacy] Sync macOS package list with homebrew package deps

--- legacy/setup-macos.sh
+++ legacy/setup-macos.sh
@@ -2,7 +2,6 @@
 
 brew update
 brew install cmake cfitsio coreutils davix icu4c fftw freetype \
-ftgl gcc giflib gl2ps gpatch graphviz grpc gsl libjpeg libpng libtiff \
-lz4 numpy openlibm openssl pcre pkg-config python sqlite \
-tbb xerces-c xrootd xxhash xz yaml-cpp zstd
-brew cask install xquartz
+  ftgl gcc giflib gl2ps glew gpatch graphviz grpc gsl libjpeg libpng \
+  libtiff libxft libxpm lz4 mesa-glu numpy openlibm openssl pcre \
+  pkg-config python sqlite tbb xerces-c xrootd xxhash xz yaml-cpp zstd
