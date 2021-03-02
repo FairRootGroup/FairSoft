2904d5d8e17bb672ebe916fcfcd82b7d2cf48dc1
Dennis Klein <d.klein@gsi.de>
2021-03-02 15:52:30 +0100

    [legayc] Bump FairMQ 1.4.32

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -176,7 +176,7 @@ ExternalProject_Add(flatbuffers
 
 if (NOT PACKAGE_SET STREQUAL fairmqdev)
   list(APPEND packages fairmq)
-  set(fairmq_version "1.4.31")
+  set(fairmq_version "1.4.32")
   ExternalProject_Add(fairmq
     GIT_REPOSITORY https://github.com/FairRootGroup/FairMQ GIT_TAG v${fairmq_version}
     ${CMAKE_DEFAULT_ARGS} CMAKE_ARGS
