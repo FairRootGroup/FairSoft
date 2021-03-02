8da08156c5b5ea3305165f953139d7f0ada2014c
Dennis Klein <d.klein@gsi.de>
2021-03-02 15:54:29 +0100

    [legacy] fairsoft-config: Set to mar21

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -385,7 +385,7 @@ if(PACKAGE_SET STREQUAL full)
   ExternalProject_Add(fairsoft-config
     GIT_REPOSITORY https://github.com/FairRootGroup/fairsoft-config GIT_TAG master
     ${CMAKE_DEFAULT_ARGS} CMAKE_ARGS
-    "-DFAIRSOFT_VERSION=nov20"
+    "-DFAIRSOFT_VERSION=mar21"
     DEPENDS root ${extract_source_cache_target}
     ${LOG_TO_FILE}
   )
