261a57e1b50b7d38c03ba1e10eb21863bcc2a9c7
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-03-31 20:44:59 +0200

    [spack] fairmq: 1.4.33, Fix fairlogger dep
    Add 1.4.33 to be in sync with legacy mar21.
    
    FairMQ 1.4.22 needs FairLogger 1.6.
    So change our depends_on accordigly.

--- repos/fairsoft/packages/fairmq/package.py
+++ repos/fairsoft/packages/fairmq/package.py
@@ -17,6 +17,7 @@ class Fairmq(CMakePackage):
     generator = 'Ninja'
 
     version('develop', branch='dev', submodules=True, get_full_repo=True)
+    version('1.4.33', tag='v1.4.33', commit='bffe74c5cfe11797adbc9acda18591ceb0275833', submodules=True, no_cache=True)
     version('1.4.30', tag='v1.4.30', commit='2b3e38d9a48a0d941a7a794be0a53c8dbfefbaec', submodules=True, no_cache=True)
     version('1.4.29', tag='v1.4.29', commit='c6b13cd3a18ac0e4d724e4df54f8027885b9ee0d', submodules=True, no_cache=True)
     version('1.4.28', tag='v1.4.28', commit='f46d446d520fab441e70decf7ab028630c9b0b96', submodules=True, no_cache=True)
@@ -76,7 +77,7 @@ class Fairmq(CMakePackage):
     depends_on('fairlogger@1.2:1.5', when='@:1.4.7')
     depends_on('fairlogger@1.2:', when='@1.4.8:1.4.10')
     depends_on('fairlogger@1.4:', when='@1.4.11:')
-    depends_on('fairlogger@1.6:', when='@develop')
+    depends_on('fairlogger@1.6:', when='@1.4.22:')
     if Version(spack_version) >= Version('0.14'):
         depends_on('libzmq@4.1.5:')
     else:
