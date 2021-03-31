4306fef5f68b672cc1b128751ccfe954a93bfeec
Dennis Klein <d.klein@gsi.de>
2021-03-31 13:26:56 +0200

    CMake: Opt-in to latest policies

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -6,7 +6,7 @@
 #                  copied verbatim in the file "LICENSE"                       #
 ################################################################################
 cmake_minimum_required(VERSION 3.16.1 FATAL_ERROR)
-cmake_policy(VERSION 3.16.1...3.19)
+cmake_policy(VERSION 3.16.1...3.20)
 if(POLICY CMP0114)
   cmake_policy(SET CMP0114 OLD)
 endif()
