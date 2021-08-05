79febea9f00030b896a9de6fe039141bcc52f5fb
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-08-05 23:23:13 +0200

    [legacy] Bump DDS to 3.5.16

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -146,7 +146,7 @@ ExternalProject_Add(fmt
 )
 
 list(APPEND packages dds)
-set(dds_version "3.5.14")
+set(dds_version "3.5.16")
 ExternalProject_Add(dds
   GIT_REPOSITORY https://github.com/FairRootGroup/DDS GIT_TAG ${dds_version}
   ${CMAKE_DEFAULT_ARGS}
