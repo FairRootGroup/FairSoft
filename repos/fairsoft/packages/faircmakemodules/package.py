e89a3a066e6c9507f017f3f28f9572dac73c9c62
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-09-08 22:28:48 +0200

    [spack] faircmakemodules: Add Version 1.0.0

--- repos/fairsoft/packages/faircmakemodules/package.py
+++ repos/fairsoft/packages/faircmakemodules/package.py
@@ -14,6 +14,7 @@ class Faircmakemodules(CMakePackage):
     maintainers = ['dennisklein', 'ChristianTackeGSI']
 
     version('master', branch='main')
+    version('1.0.0', sha256='ec60c31f38050c1173d512c58c684650db66736877c580936f7ecca33eeaf696')
     version('0.2.0', sha256='a1f8e1e022eabbc7729ff6055915a3d9c419c4f5ee7301f63df4e04c42be2edd')
     version('0.1.0', sha256='1a2be01d3f3e0d0a5bbaf0ed4a4ea8d2c2f86eeeda3158f760761b41bd568e02')
 
