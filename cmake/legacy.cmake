1328109f4b307a63ab0b1aef80988f7b693fb904
Dennis Klein <d.klein@gsi.de>
2022-04-20 19:50:16 +0200

    [legacy] fairsoft-config: Bump to apr22

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -443,7 +443,7 @@ if(PACKAGE_SET STREQUAL full)
   ExternalProject_Add(fairsoft-config
     GIT_REPOSITORY https://github.com/FairRootGroup/fairsoft-config GIT_TAG master
     ${CMAKE_DEFAULT_ARGS} CMAKE_ARGS
-    "-DFAIRSOFT_VERSION=mar22"
+    "-DFAIRSOFT_VERSION=apr22"
     DEPENDS root ${extract_source_cache_target}
     ${LOG_TO_FILE}
   )
