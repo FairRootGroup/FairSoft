8b430530ed5e57048061ae8561105d90d3a9db69
Florian Uhlig <f.uhlig@gsi.de>
2022-10-18 14:14:54 +0200

    [legacy] onnxruntime: Add patchset for CMake config files

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -429,6 +429,7 @@ ExternalProject_Add(geant4_vmc
 list(APPEND packages onnxruntime)
 set(onnxruntime_version "1.12.1")
 ExternalProject_Add(onnxruntime
+  PATCH_COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/onnxruntime/install_config_files.patch"
   GIT_REPOSITORY https://github.com/microsoft/onnxruntime/ GIT_TAG v${onnxruntime_version}
   GIT_SHALLOW ON
   GIT_SUBMODULES
