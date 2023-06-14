24ed45987708417c5668b6fff4d886f45303a33b
Dennis Klein <d.klein@gsi.de>
2023-06-14 19:42:25 +0200

    [legacy] Adopt latest CMake policies

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -5,8 +5,7 @@
 #              GNU Lesser General Public Licence (LGPL) version 3,             #
 #                  copied verbatim in the file "LICENSE"                       #
 ################################################################################
-cmake_minimum_required(VERSION 3.18 FATAL_ERROR)
-cmake_policy(VERSION 3.18...3.24)
+cmake_minimum_required(VERSION 3.18...3.28 FATAL_ERROR)
 if(POLICY CMP0114)
   cmake_policy(SET CMP0114 OLD)
 endif()
