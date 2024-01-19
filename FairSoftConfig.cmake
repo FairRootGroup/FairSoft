d56901dc4a0c22949956c7c66b0daf9132ba63e7
Florian Uhlig <f.uhlig@gsi.de>
2024-01-19 11:27:35 +0100

    [legacy] Allow boost to find external libs
    When brew installs software to a differant directory than /usr/local like on
    Apple silicon boost can't find the packages. So pass the proper information to
    the boost build process on this platform.

--- FairSoftConfig.cmake
+++ FairSoftConfig.cmake
@@ -79,3 +79,79 @@ if(APPLE)
   string(STRIP "${macos_sdk_path}" macos_sdk_path)
   set(CMAKE_OSX_SYSROOT "${macos_sdk_path}" CACHE FILEPATH "macOS SDK" FORCE)
 endif()
+
+if(APPLE)
+  execute_process(COMMAND brew --prefix OUTPUT_VARIABLE brew_prefix)
+endif()
+
+#
+# LZMA
+#
+# On macOS LZMA isn't installed in some standard system path
+# so we need to take it from brew. The xz package which contains
+# the needed headers and libraries is installed with the setup script
+# from <source-dir>/legacy/setup-macos.sh.
+# On Linux the packages are in the default system paths and are found
+# automatically by Boost.
+#
+if(NOT ${brew_prefix} MATCHES "/usr/local")
+  execute_process(COMMAND brew list xz RESULT_VARIABLE lzma_installed OUTPUT_VARIABLE _tmp)
+  if (NOT lzma_installed)
+    message(STATUS "LZMA installation found. Boost iostream will be build with lzma compression")
+    execute_process(COMMAND brew --prefix xz OUTPUT_VARIABLE lzma_prefix)
+    string(STRIP "${lzma_prefix}" lzma_prefix)
+    set(LZMA_ROOT "${lzma_prefix}" CACHE FILEPATH "LZMA prefix" FORCE)
+    execute_process(COMMAND ${LZMA_ROOT}/bin/xz --version
+                    OUTPUT_VARIABLE lzma_version
+                   )
+    string(REGEX REPLACE ".*([0-9]\\.[0-9]\\.[0-9])+.*" "\\1" lzma_version1 ${lzma_version})
+    set(LZMA_EXTERNAL "using lzma   :" CACHE STRING "LZMA exetrnal usage for Boost" FORCE)
+    set(LZMA_VERSION "${lzma_version1}" CACHE STRING "LZMA version info for Boost" FORCE)
+    set(LZMA_STRING ": <include>${LZMA_ROOT}/include <search>${LZMA_ROOT}/lib ;" CACHE STRING "LZMA install info for Boost" FORCE)
+  else()
+    message(WARNING "LZMA installation not found. Boost iostream will be build without lzma compression")
+    set(LZMA_EXTERNAL "" CACHE STRING "LZMA exetrnal usage for Boost" FORCE)
+    set(LZMA_VERSION "" CACHE STRING "LZMA version info for Boost" FORCE)
+    set(LZMA_STRING "" CACHE STRING "LZMA install info for Boost" FORCE)
+  endif()
+else()
+  set(LZMA_EXTERNAL "" CACHE STRING "LZMA exetrnal usage for Boost" FORCE)
+  set(LZMA_VERSION "" CACHE STRING "LZMA version info for Boost" FORCE)
+  set(LZMA_STRING "" CACHE STRING "LZMA install info for Boost" FORCE)
+endif()
+
+#
+# ZSTD
+#
+# On macOS ZSTD isn't installed in some standard system path
+# so we need to take it from brew. The zstd package which contains
+# the needed headers and libraries is installed with the setup script
+# from <source-dir>/legacy/setup-macos.sh.
+# On Linux the packages are in the default system paths and are found
+# automatically by Boost.
+#
+if(NOT ${brew_prefix} MATCHES "/usr/local")
+  execute_process(COMMAND brew list zstd RESULT_VARIABLE zstd_installed OUTPUT_VARIABLE _tmp)
+  if (NOT zstd_installed)
+    message(STATUS "ZSTD installation found. Boost iostream will be build with zstd compression")
+    execute_process(COMMAND brew --prefix zstd OUTPUT_VARIABLE zstd_prefix)
+    string(STRIP "${zstd_prefix}" zstd_prefix)
+    set(ZSTD_ROOT "${zstd_prefix}" CACHE FILEPATH "ZSTD prefix" FORCE)
+    execute_process(COMMAND ${ZSTD_ROOT}/bin/zstd --version
+                    OUTPUT_VARIABLE zstd_version
+                   )
+    string(REGEX REPLACE "^.*([0-9]\\.[0-9]\\.[0-9])+.*" "\\1" zstd_version1 "${zstd_version}")
+    set(ZSTD_EXTERNAL "using zstd   :" CACHE STRING "ZSTD exetrnal usage for Boost" FORCE)
+    set(ZSTD_VERSION "${zstd_version1}" CACHE STRING "ZSTD version info for Boost" FORCE)
+    set(ZSTD_STRING ": <include>${ZSTD_ROOT}/include <search>${ZSTD_ROOT}/lib ;" CACHE STRING "ZSTD install info for Boost" FORCE)
+  else()
+    message(WARNING "ZSTD installation not found. Boost iostream will be build without zstd compression")
+    set(ZSTD_EXTERNAL "" CACHE STRING "ZSTD exetrnal usage for Boost" FORCE)
+    set(ZSTD_VERSION "" CACHE STRING "ZSTD version info for Boost" FORCE)
+    set(ZSTD_STRING "" CACHE STRING "ZSTD install info for Boost" FORCE)
+  endif()
+else()
+  set(ZSTD_EXTERNAL "" CACHE STRING "ZSTD exetrnal usage for Boost" FORCE)
+  set(ZSTD_VERSION "" CACHE STRING "ZSTD version info for Boost" FORCE)
+  set(ZSTD_STRING "" CACHE STRING "ZSTD install info for Boost" FORCE)
+endif()
