9d2d005bbe30e12d79ecd510e43f90ffda35641d
Florian Uhlig <f.uhlig@gsi.de>
2024-06-10 09:49:57 +0200

    [legacy] Add test for FairRoot v19.0
    (cherry picked from commit 8b0b3cc7867d21802ce220d6aedd7a5f4c00b6ba)

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -514,7 +514,7 @@ add_custom_target(source-cache
 
 include(CTest)
 
-foreach(ver IN ITEMS 18.4 18.6 18.8)
+foreach(ver IN ITEMS 18.4 18.6 18.8 19.0)
   set(TEST_VERSION v${ver}_patches)
   configure_file(test/legacy/fairroot.sh.in ${CMAKE_BINARY_DIR}/test_fairroot_${ver}.sh @ONLY)
   add_test(NAME FairRoot_${ver}
