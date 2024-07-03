ecb25c792a4a197cc7966f959d50813108893733
Dennis Klein <d.klein@gsi.de>
2024-07-03 22:00:46 +0200

    [legacy] vc: Bump v1.4.5

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -283,10 +283,10 @@ ExternalProject_Add(hepmc
 )
 
 list(APPEND packages vc)
-set(vc_version "1.4.4")
+set(vc_version "1.4.5")
 ExternalProject_Add(vc
   URL https://github.com/VcDevel/Vc/archive/refs/tags/${vc_version}.tar.gz
-  URL_HASH SHA256=5933108196be44c41613884cd56305df320263981fe6a49e648aebb3354d57f3
+  URL_HASH SHA256=eb734ef4827933fcd67d4c74aef54211b841c350a867c681c73003eb6d511a48
   ${CMAKE_DEFAULT_ARGS} ${LOG_TO_FILE}
   ${DEPENDS_ON_SOURCE_CACHE}
 )
