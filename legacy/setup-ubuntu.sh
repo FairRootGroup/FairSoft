21fff175615d621a43b959791eeeea649777d90c
Florian Uhlig <f.uhlig@gsi.de>
2024-11-14 08:30:09 +0100

    [legacy] add packages to setup files
    With newer versions of Linux like Fedora 41 or Ubuntu 24.04 the compilation of
    some ROOT packages like xrootd failed. Add those packages from the system to
    avoid compiling them with ROOT.
    Add zstd and sqlite development packages needed by CBM.

--- legacy/setup-ubuntu.sh
+++ legacy/setup-ubuntu.sh
@@ -9,9 +9,11 @@ apt-get -y install autoconf automake binutils \
 	hostname libbz2-dev libcurl4-openssl-dev libgsl-dev libicu-dev \
 	libfftw3-dev \
 	libgl1-mesa-dev libglu1-mesa-dev libgrpc++-dev \
-	libncurses-dev libreadline-dev libsqlite3-dev libssl-dev libtool \
+	libncurses-dev libreadline-dev libsqlite3-dev libssl-dev libtbb-dev libtool \
 	libx11-dev libxerces-c-dev libxext-dev libxft-dev \
-	libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev lsb-release make patch \
-	python3-dev protobuf-compiler-grpc rsync sed subversion tar unzip wget xutils-dev xz-utils
+	libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev libzstd-dev lsb-release make patch \
+	python3-dev protobuf-compiler-grpc rsync sed subversion tar unzip wget xutils-dev \
+	libxrootd-server-dev libxrootd-private-dev xz-utils
 apt-get -y install cmake
 apt-get -y clean
+
