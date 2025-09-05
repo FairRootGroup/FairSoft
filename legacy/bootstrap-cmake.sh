02adc68f2006168233831a3cbc14c50cbb925e13
Florian Uhlig <f.uhlig@gsi.de>
2025-09-05 11:47:06 +0200

    [legacy] bootstrap-cmake.sh: Bump to v4.2.3
    The name of the tar file as well as the content has changed on macOS.
    Adapt the installation to those changes.

--- legacy/bootstrap-cmake.sh
+++ legacy/bootstrap-cmake.sh
@@ -9,7 +9,13 @@ then
   echo "Usage: bootstrap-cmake.sh INSTALLDIR"
   exit 1
 else
-  installdir=$(realpath $1)
+  installdir=$(realpath "$1")
+  if [[ $? -ne 0 ]]; then
+    echo ""
+    echo "Install directory $installdir must exist on macOS."
+    echo "Please create the directory."
+    exit 1
+  fi
 fi
 
 set -e
@@ -19,10 +25,17 @@ trap "cleanup ${builddir}" EXIT
 pushd "${builddir}" > /dev/null
 
 cmakebaseurl="https://github.com/Kitware/CMake/releases/download/v"
-cmakeversion="3.27.4"
+cmakeversion="4.2.3"
 unames=$(uname -s | tr '[:upper:]' '[:lower:]')
 unamem=$(uname -m | tr '[:upper:]' '[:lower:]')
-cmaketargz="cmake-${cmakeversion}-${unames}-${unamem}.tar.gz"
+if [[ "$unames" == "darwin" ]]; then
+  cmaketargz="cmake-${cmakeversion}-macos-universal.tar.gz"
+  grepstring="macos-universal.*\.tar\.gz"
+else
+  cmaketargz="cmake-${cmakeversion}-${unames}-${unamem}.tar.gz"
+  grepstring="${unames}.*${unamem}.*\.tar\.gz"
+fi
+
 cmakechecksums="cmake-${cmakeversion}-SHA-256.txt"
 
 cmake="${cmakebaseurl}${cmakeversion}/$cmaketargz"
@@ -30,12 +43,20 @@ echo "Downloading ${cmake}"
 curl -L -O -# "${cmake}"
 
 curl -L -O -s "${cmakebaseurl}${cmakeversion}/$cmakechecksums"
-grep "${unames}.*${unamem}.*\.tar\.gz" $cmakechecksums > checksum.txt
+grep "$grepstring" $cmakechecksums > checksum.txt
 sha256sum -c checksum.txt
 
 echo "Installing to ${installdir}"
-mkdir -p "${installdir}"
-tar xf "$cmaketargz" -C "$installdir" --strip-components=1
+if [[ "$unames" == "darwin" ]]; then
+  tar xf "$cmaketargz" -C "$PWD" --strip-components=1
+  cp -r CMake.app/Contents/bin "${installdir}"
+  cp -r CMake.app/Contents/doc "${installdir}"
+  cp -r CMake.app/Contents/man "${installdir}"
+  cp -r CMake.app/Contents/share "${installdir}"
+else
+  mkdir -p "${installdir}"
+  tar xf "$cmaketargz" -C "$installdir" --strip-components=1
+fi
 
 echo "Use via"
 echo "  export PATH=${installdir}/bin:\$PATH"
