cb4020970b67acd2f5713479f64f175689f4b0fc
Florian Uhlig <f.uhlig@gsi.de>
2025-09-08 09:36:42 +0200

    [legacy] Update setup scripts
    Add additional packages.

--- legacy/setup-debian.sh
+++ legacy/setup-debian.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
 apt-get -y install autoconf automake binutils \
-	bison build-essential bzip2 ca-certificates coreutils \
-	curl debianutils file findutils flex g++ gcc gfortran git gzip \
-	hostname libbz2-dev libcurl4-openssl-dev libgsl-dev libicu-dev \
-	libfftw3-dev libprotobuf-dev \
-	libgl1-mesa-dev libglu1-mesa-dev libgrpc++-dev \
-	liblzma-dev libncurses-dev libreadline-dev libsqlite3-dev libssl-dev libtool \
-	libx11-dev libxerces-c-dev libxext-dev libxft-dev \
-	libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev libzstd-dev lsb-release make patch \
-	python3-dev protobuf-compiler-grpc rsync sed subversion tar unzip wget xutils-dev xz-utils
-apt-get -y -t buster-backports install cmake || \
-	apt-get -y install cmake
+        bison build-essential bzip2 ca-certificates cmake coreutils \
+        curl debianutils file findutils flex g++ gcc gfortran git gzip \
+        hostname libbz2-dev libcurl4-openssl-dev libgsl-dev libicu-dev \
+        libfftw3-dev libprotobuf-dev \
+        libgl1-mesa-dev libglu1-mesa-dev libgrpc++-dev \
+        liblzma-dev libncurses-dev libreadline-dev libsqlite3-dev libssl-dev libtool \
+        libx11-dev libxerces-c-dev libxext-dev libxft-dev \
+        libxml2-dev libxmu-dev libxpm-dev libyaml-cpp-dev libzstd-dev lsb-release make patch \
+        python3-dev protobuf-compiler-grpc rsync sed subversion tar unzip wget xutils-dev xz-utils 
 apt-get -y clean
--- legacy/setup-opensuse.sh
+++ legacy/setup-opensuse.sh
@@ -1,15 +1,34 @@
 #! /bin/bash
 
-zypper refresh
-zypper update -y
-zypper install -y autoconf automake binutils bison bzip2 libbz2-devel cmake \
-ca-certificates coreutils curl libcurl-devel diffutils expat libexpat-devel \
-fftw-devel findutils flex gcc-c++ gcc-fortran gdbm-devel gettext-devel git gperf gsl-devel \
-gzip help2man hostname hwloc hwloc-devel m4 make libbsd-devel libtool libicu-devel \
-glu-devel grpc-devel libunistring-devel libuuid-devel libX11-devel libXau-devel \
-libXdmcp-devel libXext-devel libXfont-devel libXft-devel libxml2-devel \
-libXmu-devel libXpm-devel libXrender-devel liblz4-devel libzstd-devel lsb-release Mesa-devel \
-ncurses-devel openssl-devel patch procps python python-devel python3-devel \
-readline-devel rsync sed subversion tar libtbb2 tbb-devel unzip wget which \
-libxerces-c-devel xz xz-devel yaml-cpp-devel
+
+zypper refresh && zypper update -y
+zypper install --no-recommends -y \
+      autoconf automake binutils bison bzip2 libbz2-devel cmake \
+      ca-certificates coreutils curl libcurl-devel diffutils expat libexpat-devel \
+      fftw-devel findutils flex gcc-c++ gcc-fortran gdbm-devel gettext-devel git gperf gsl-devel \
+      gzip help2man hostname htop hwloc hwloc-devel joe m4 make libbsd-devel libtool libicu-devel \
+      glu-devel grpc-devel libunistring-devel libuuid-devel libX11-devel libXau-devel \
+      libXdmcp-devel libXext-devel libXfont-devel libXft-devel libxml2-devel \
+      libXmu-devel libXpm-devel libXrender-devel liblz4-devel libzstd-devel lsb-release Mesa-devel \
+      ncurses-devel openssl-devel patch procps protobuf-devel python3 python3-devel \
+      readline-devel rsync sed subversion tar libtbb2 tbb-devel unzip wget which \
+      libxerces-c-devel xz xz-devel yaml-cpp-devel
 zypper clean
+
+os_version=$(cat /etc/os-release | grep 'VERSION=' | cut -d\" -f2)
+if [[ "${os_version}" == "15.6" ]]; then
+      # install non system compiler which is needed to compile FairSoft for OpenSuse Leap 15.6
+      # install newer python3 version which is needed for onnxruntime
+      # install protobuf development files which is needed by dds
+      zypper install --no-recommends -y \
+         gcc14 gcc14-c++ gcc14-fortran \
+         python310 python310-devel \
+         libprotobuf-c-devel 
+         
+      zypper remove -y python3 python3-devel 
+      rm /usr/bin/python3
+      ln -s /usr/bin/python3.10 /usr/bin/python3
+elif [[ "${os_version}" == "16.0" ]]; then
+     zypper install --no-recommends -y \
+         protobuf-c-devel
+fi
