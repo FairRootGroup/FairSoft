681d37c2fee369f96d9b0af89e23ac2f51a3dd23
Florian Uhlig <f.uhlig@gsi.de>
2026-02-11 16:43:49 +0100

    [legacy] onnxruntime: Bump 1.24.1
    In contrast to the default packages onnxruntime needs newer versions of
    CMake (cmake >=3.28) and the compiler (gcc >= 11.1).
    For the optional package this is tolerated. The error messages are
    meaningful and point to the problem of insufficient versions.
    Do not search for any packages in the system for packages added using
    FetchContent. The packages included with onnxruntime are verry
    specific and may interfere with system installed packages.
    Add patch needed for Fedora 38.
    
    Fixes https://github.com/FairRootGroup/FairSoft/issues/510

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -522,27 +522,21 @@ ExternalProject_Add(geant4_vmc
 )
 
 list(APPEND packages onnxruntime)
-set(onnxruntime_version "1.12.1")
+set(onnxruntime_version "1.24.1")
 ExternalProject_Add(onnxruntime
   UPDATE_DISCONNECTED ON
-  PATCH_COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/onnxruntime/install_config_files.patch"
-  COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/onnxruntime/fix_python_detection.patch"
+  PATCH_COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/onnxruntime/fix_gcc13_compilation.patch"
   GIT_REPOSITORY https://github.com/microsoft/onnxruntime/ GIT_TAG v${onnxruntime_version}
   GIT_SHALLOW ON
   GIT_SUBMODULES
-    "cmake/external/SafeInt"
-    "cmake/external/date"
-    "cmake/external/flatbuffers" # at the moment, there is no option to consume external
-    "cmake/external/json"
-    "cmake/external/mp11"
-    "cmake/external/nsync"
     "cmake/external/onnx"
-    "cmake/external/protobuf" # TODO explore, if we can offer this as separate pkg (conditionally?)
-    "cmake/external/pytorch_cpuinfo"
-    "cmake/external/re2"
-    "cmake/external/eigen"
+    "cmake/external/emsdk"
+    "cmake/external/libprotobuf-mutator"
   SOURCE_SUBDIR cmake
   ${CMAKE_DEFAULT_ARGS} CMAKE_ARGS
+    "-DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=NEVER"
+    "-UCMAKE_PREFIX_PATH"
+    "-DCMAKE_BUILD_TYPE=Release"
     "-Donnxruntime_BUILD_UNIT_TESTS=OFF"
     "-Donnxruntime_BUILD_SHARED_LIB=ON"
   DEPENDS ${extract_source_cache_target}
